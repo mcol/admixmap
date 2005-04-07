8c79b4eba7f0dc7febda66bdc9f9d964063c497c
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2005-04-07 12:35:58 +0000

    added comments

--- gslwrap-ch/rand.h
+++ gslwrap-ch/rand.h
@@ -20,17 +20,26 @@ unsigned int genpoi( double );
 //
 Vector_i genmultinomial( int n, Vector_d p );
 //
+// multinomial probability of counts r given probability vector theta
 double MultinomialLikelihood( Vector_i r, Vector_d theta );
 ///
 ///Poisson generator
 long ignpoi(double mu);
 ///
+// sampler with cumulative probs as argument
 int SampleFromDiscrete( Vector_d *cdf );
 ///
+// sampler with probs as argument
 int SampleFromDiscrete2( Vector_d *probs );
 ///
+/// sample proportion vector from Dirichlet distribution 
+// Dirichlet parameter vector may contain zeroes
 Vector_d gendirichlet( Vector_d );
 ///
+// these next two functions don't really belong in the rand.h file
+// 
+// digamma function: first parameter is function argument, second parameter is result 
 void ddigam( double *, double * );
 ///
+// trigamma function
 void trigam( double *, double * );
