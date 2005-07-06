96a78b6f75c36bbab54fba04f4d3aa5f34932fb2
clive <clive@0c12335c-6928-0410-89ea-c6d2b26f428b>
2005-07-06 15:15:57 +0000

    added comments

--- admixmap/TuneRW.h
+++ admixmap/TuneRW.h
@@ -5,26 +5,26 @@
 class TuneRW
 {
 public:
+    TuneRW(int w, double sigma0, double min, double max, double target);
     TuneRW();
-    TuneRW(int, double, double, double, double);
     ~TuneRW();
 
-    void SetParameters(int, double, double, double, double);
+    void SetParameters(int w, double sigma0, double min, double max, double target);
     
     double GetSigma();
-    double UpdateSigma(int);
+    double UpdateSigma(int NumberAccepted);
     void   Event(bool);
 
 private:
-    double sigma0;
-    double sigma;
-    double min;
-    double max;
-    double target;
-    int k;
-    int w;
-    int count;
-    int NumberAccepted;
+   double sigma0; // Initial value of parameter of random walk being tuned.
+   double sigma; // Current value of parameter of random walk being tuned.
+   double min; // Minimum value of sigma
+   double max; // Maximum value of sigma
+   double target; // Target acceptance probability
+   int k; // Number of times sigma sigma updated
+   int w; // Frequency sigma is updated - 10 is good
+   int count; // Number of iterations since sigma lsat updated
+   int NumberAccepted; // Number of accepted proposals in random-walk since sgma last updated
 };
 
 #endif /* ! TuneRW_H */
