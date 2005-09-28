20d2342830f80dfe4ae198a9244a3d4c0e636f50
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2005-09-28 14:33:19 +0000

    no message

--- sim/SimStratified.R
+++ sim/SimStratified.R
@@ -48,7 +48,7 @@ numChr <- 22
 ## chromosome lengths in cM
 chr.L <- c(292,272,233,212,197,201,184,166,166,181,156,169,117,128,110,130,128,123,109,96,59,58)
 N <- 500
-numsims <- 1
+numsims <- 100
 NumSubPops <- 2 # num subpopulations
 popadmixparams <- c(1, 2) # population admixture params for pop1, pop2
 rho <- 6 # sum-of-intensities
