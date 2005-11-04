ad3db8445cf387a42d4efda78d48921a54d4b062
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2005-11-04 09:35:10 +0000

    no message

--- tutorial/tutorial.pl
+++ tutorial/tutorial.pl
@@ -1,52 +1,46 @@
 #!/usr/bin/perl
 use strict; 
 use File::Path;
-
 ################### DO NOT EDIT ABOVE THIS LINE ########################
 
 # Change this to the location of the admixmap executable
-my $executable = '../test/admixmap.exe';
+my $executable = '../test/admixmap';
 # command-line options are stored in an associative array (known as a hash in perl)  
-my $arg_hash = 
-{
+my $arg_hash = {
 #data files
     genotypesfile                   => 'data/genotypes.txt',
     locusfile                       => 'data/loci.txt',
     #covariatesfile                  => 'data/covariates3std.txt', # age, sex, income
     covariatesfile                  => 'data/covariates2std.txt', # age, sex only
     outcomevarfile                  => 'data/outcomevars.txt',
-    
 #main options
     samples  => 1200,
     burnin   => 200,
     every    => 5,
-    coutindicator => 0, 
-    
+    coutindicator => 1, 
 #output file options
     logfile                     => 'log.txt',
     ergodicaveragefile          => 'cumulativeresults.txt',
     paramfile                   => 'popadmixparams.txt',
     regparamfile                => 'regparam.txt',
-    
+    ergodicaveragefile          => 'cumulativeAverages.txt',
 # optional tests
     allelicassociationscorefile       => 'allelicassociationscoretests.txt',
     haplotypeassociationscorefile     => 'hapassocscoretests.txt',
     stratificationtestfile            => 'stratificationtest.txt',
-    hwscoretestfile                   => 'HardyWeinbergTest.txt'
+    hwscoretestfile                   => 'HardyWeinbergTests.txt'
 };
 
 # model with reference prior on allele freqs in 1 population, skin reflectance as continuous outcome var
 $arg_hash->{populations}           = 1;
-$arg_hash->{resultsdir}            = 'SinglePopResults';  
-$arg_hash->{targetindicator}       = 1; # skin reflectance as outcome
+$arg_hash->{resultsdir}            = 'SinglePopResults';
+$arg-hash->{outcomes}              = 1,
+$arg_hash->{targetindicator}       = 1; # skin reflectance
 doAnalysis($executable,$arg_hash);
 
 # model with reference prior on allele freqs in 2 populations
 $arg_hash->{populations}           = 2;
 $arg_hash->{resultsdir}            = 'TwoPopsResults';  
-#$arg_hash->{samples}               = 5500;
-#$arg_hash->{burnin}                = 500;
-#$arg_hash->{every}                 = 5;
 doAnalysis($executable,$arg_hash);
 
 # model with reference prior on allele freqs in 3 populations
@@ -80,8 +74,8 @@ delete $arg_hash->{dispersiontestfile};
 delete $arg_hash->{affectedsonlyscorefile};
 $arg_hash->{resultsdir}                = 'HistoricFreqResults';  
 $arg_hash->{randommatingmodel}         = 1;
-$arg_hash->{outcomes}=2;
-$arg_hash->{historicallelefreqfile}    = "data/priorallelefreqs.txt";
+$arg_hash->{outcomes}                  = 2; # both outcome vars
+$arg_hash->{historicallelefreqfile}    = "data/priorallelefreqsnew.txt";
 $arg_hash->{etapriorfile}              = "data/etapriors.txt";
 $arg_hash->{dispparamfile}             = "dispersionparams.txt";
 $arg_hash->{fstoutputfile}             = "lociFst.txt";
@@ -90,8 +84,7 @@ doAnalysis($executable,$arg_hash);
 
 ############### DO NOT EDIT BELOW THIS LINE ############################
 
-sub getArguments
-{
+sub getArguments {
     my $hash = $_[0];
     my $arg = '';
     foreach my $key (keys %$hash){
@@ -100,8 +93,7 @@ sub getArguments
     return $arg;
 }
 
-sub doAnalysis
-{
+sub doAnalysis {
     my ($prog,$args) = @_;
     my $command = $prog.getArguments($args);
     if (-e $args->{resultsdir}) {
@@ -112,7 +104,8 @@ sub doAnalysis
     print "Results will be written to subdirectory $ENV{'RESULTSDIR'}\n";
     system($command);
     print "Starting R script to process output\n";
-    system("RCMD BATCH --quiet --no-save --no-restore ../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt");
+    system("RCMD BATCH --quiet --no-save --no-restore \
+            ../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt");
     print "R script completed\n\n";
 }
 
