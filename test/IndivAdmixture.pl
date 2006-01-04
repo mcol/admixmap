f81287c2074d4ce77ca168f41a3aeed1115c6780
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-01-04 14:26:26 +0000

    no message

--- test/IndivAdmixture.pl
+++ test/IndivAdmixture.pl
@@ -1,18 +1,12 @@
 #!/usr/bin/perl -w
+use strict; 
+use File::Path;
 
-# Change this to the location of the admixmap executable
 my $executable = './admixmap';
-my $results = "IndResults";
 
-# $arg_hash is a hash of parameters passed to
-# the executable as arguments.
-#
-# keys (left-hand side) are parameter names
-# values (right-hand side) are parameter values
-my $arg_hash = 
-{
-    burnin   => 500, 
-    samples  => 4500,
+my $arg_hash = {
+    burnin   => 50, 
+    samples  => 450,
     every    => 10,
     locusfile                    => "IndData/loci.txt",
     genotypesfile                => "IndData/genotypes.txt",
@@ -22,43 +16,25 @@ my $arg_hash =
     initalpha0                   => "1,1,1", # parameter vectors for Dirichlet prior on admixture 
     initalpha1                   => "1,1,0",
     chib                         => 1,
-    #thermo                       => 1,
-    #numannealedruns             => 100,
+    thermo                       => 1,
+    numannealedruns             => 100,
 
     resultsdir => "IndResults",
     logfile                      => "logfile.txt",
-    paramfile                    => "paramfile.txt",
     indadmixturefile             => "indadmixture.txt"
 };
 
-$arg_hash->{resultsdir} = "IndResults2Way";
-doAnalysis($executable,$arg_hash, "IndResults2Way");
+$arg_hash->{initalpha0} = "0,1,0";
+$arg_hash->{initalpha1} = "1,0,0";
+$arg_hash->{resultsdir} = "IndResults010-100";
+&doAnalysis($executable, $arg_hash);
 
-$arg_hash->{initalpha0} = "1,1,1";
-$arg_hash->{initalpha1} = "1,1,1";
-$arg_hash->{resultsdir} = "IndResults3Way";
-#doAnalysis($executable,$arg_hash, "IndResults3Way");
+$arg_hash->{initalpha0} = "0,1,0";
+$arg_hash->{initalpha1} = "1,1,0";
+$arg_hash->{resultsdir} = "IndResults010-110";
+&doAnalysis($executable, $arg_hash);
 
-$arg_hash->{fixedallelefreqs} = 1;
-$arg_hash->{resultsdir} = "IndResults3WayFixed";
-#doAnalysis($executable,$arg_hash, "IndResults3WayFixed");
-
-
-sub doAnalysis
-{
-    my ($prog,$args, $resultsdir) = @_;
-    my $command = $prog.getArguments($args);
-    unless (-e "$resultsdir"){
-	system("mkdir $resultsdir");
-    }
-    system($command);
-    print "Starting R script to process output\n";
-     system("R --quiet --no-save --no-restore <AdmixmapOutput.R >results/Rlog.txt RESULTSDIR=$resultsdir");
-    print "R script completed\n\n";
-}
-
-sub getArguments
-{
+sub getArguments {
     my $hash = $_[0];
     my $arg = '';
     foreach my $key (keys %$hash){
@@ -67,14 +43,22 @@ sub getArguments
     return $arg;
 }
 
-sub RenameDir{
-    my($source, $target) = @_;
-    if ($^O eq "MSWin32") {
-	if (-e $target) {system("rmdir /s /q $target")}
-	system("move /y $source $target");
+sub doAnalysis {
+    my ($prog,$args) = @_;
+    my $command = $prog.getArguments($args);
+    if (-e $args->{resultsdir}) {
+	rmtree($args->{resultsdir});
+    } 
+    mkpath($args->{resultsdir});
+    $ENV{'RESULTSDIR'} = $args->{resultsdir};
+    print "\nResults will be written to subdirectory $ENV{'RESULTSDIR'}\n";
+    system($command);
+    my $rcmd = "R CMD";
+    if($^O eq "MSWin32") {
+	$rcmd = "Rcmd";
     }
-    else {
-	if (-e $target) {system("rm -r $source")}
-   	system("mv -f  $source $target");
-    }
+    print "Starting R script to process output\n";
+    system("$rcmd BATCH --quiet --no-save --no-restore ../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt\n");
+    print "R script completed\n\n";
 }
+
--- tutorial/tutorial.pl
+++ tutorial/tutorial.pl
@@ -10,18 +10,17 @@ my $arg_hash = {
 #data files
     genotypesfile                   => 'data/genotypes.txt',
     locusfile                       => 'data/loci.txt',
-    #covariatesfile                  => 'data/covariates3std.txt', # age, sex, income
-    covariatesfile                  => 'data/covariates2std.txt', # age, sex only
+    covariatesfile                  => 'data/covariates2std.txt', # age, sex 
     outcomevarfile                  => 'data/outcomevars.txt',
 #main options
-    samples  => 1200,
-    burnin   => 200,
+    samples  => 550,
+    burnin   => 50,
     every    => 5,
-    coutindicator => 0, 
+    numannealedruns => 0, 
+    #displaylevel => 3,
+    coutindicator => 1, 
 #output file options
     logfile                     => 'log.txt',
-    ergodicaveragefile          => 'cumulativeresults.txt',
-    paramfile                   => 'popadmixparams.txt',
     regparamfile                => 'regparam.txt',
     ergodicaveragefile          => 'cumulativeAverages.txt',
 # optional tests
@@ -36,17 +35,18 @@ $arg_hash->{populations}           = 1;
 $arg_hash->{resultsdir}            = 'SinglePopResults';
 $arg_hash->{outcomes}              = 1,
 $arg_hash->{targetindicator}       = 1; # skin reflectance
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 # model with reference prior on allele freqs in 2 populations
 $arg_hash->{populations}           = 2;
+$arg_hash->{paramfile}                 = 'popadmixparams.txt',
 $arg_hash->{resultsdir}            = 'TwoPopsResults';  
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 # model with reference prior on allele freqs in 3 populations
 $arg_hash->{populations}           = 3;
 $arg_hash->{resultsdir}            = 'ThreePopsResults';  
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 # model with prior allele freqs 
 delete $arg_hash->{populations};
@@ -55,29 +55,29 @@ $arg_hash->{priorallelefreqfile}           = 'data/priorallelefreqs.txt';
 $arg_hash->{dispersiontestfile}            = 'dispersionTest.txt';
 $arg_hash->{indadmixturefile}              = 'indivadmixture.txt';
 $arg_hash->{ancestryassociationscorefile}  = 'ancestryassociationscorefile.txt';
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 # model with prior allele freqs and diabetes as binary outcome var 
 delete $arg_hash->{populations};
 $arg_hash->{resultsdir}                = 'PriorFreqResultsDiabetes';  
 $arg_hash->{targetindicator}           = 0; # diabetes as outcome
-$arg_hash->{outcomes} = 1;
 $arg_hash->{affectedsonlyscorefile}    = 'affectedsonlyscorefile.txt';
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 # model with historic allele freqs and both outcome vars
+delete $arg_hash->{targetindicator};  
 delete $arg_hash->{priorallelefreqfile};
 delete $arg_hash->{dispersiontestfile};
 delete $arg_hash->{affectedsonlyscorefile};
 $arg_hash->{resultsdir}                = 'HistoricFreqResults';  
 $arg_hash->{randommatingmodel}         = 1;
-$arg_hash->{outcomes}                  = 2; # both outcome vars
+$arg_hash->{outcomes}                  = 2; # both outcome vars - overrides targetindicator
 $arg_hash->{historicallelefreqfile}    = "data/priorallelefreqs.txt";
 $arg_hash->{etapriorfile}              = "data/etapriors.txt";
 $arg_hash->{dispparamfile}             = "dispersionparams.txt";
 $arg_hash->{fstoutputfile}             = "lociFst.txt";
 $arg_hash->{allelefreqoutputfile}      = "allelefreqs.txt";
-doAnalysis($executable,$arg_hash);
+&doAnalysis($executable,$arg_hash);
 
 ############### DO NOT EDIT BELOW THIS LINE ############################
 
@@ -98,18 +98,14 @@ sub doAnalysis {
     } 
     mkpath($args->{resultsdir});
     $ENV{'RESULTSDIR'} = $args->{resultsdir};
+    print "\nResults will be written to subdirectory $ENV{'RESULTSDIR'}\n";
+    system($command);
     my $rcmd = "R CMD";
     if($^O eq "MSWin32") {
 	$rcmd = "Rcmd";
     }
-    print "Results will be written to subdirectory $ENV{'RESULTSDIR'}\n";
-     system($command);
     print "Starting R script to process output\n";
-    print("$rcmd BATCH --quiet --no-save --no-restore ../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt\n");
     system("$rcmd BATCH --quiet --no-save --no-restore ../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt\n");
     print "R script completed\n\n";
 }
-
-
-
 
