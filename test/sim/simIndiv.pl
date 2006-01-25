633cccf9498acaaaa942898c66ea7c32223a1a00
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-01-25 17:16:37 +0000

    no message

---
+++ test/sim/simIndiv.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict; 
+use File::Path;
+
+################### DO NOT EDIT ABOVE THIS LINE ########################
+# Change this to the name of the program to be run
+my $executable = 'c:/cvs/genepi/test/admixmap';
+
+my $arg_hash = 
+{
+    burnin   => 50,
+    samples  => 1050,
+    every    => 5,
+    indadmixhiermodel            => 0,
+    chib                         => 1,
+    randommatingmodel            => 1,
+    globalrho                    => 0,
+    locusfile                    => 'data/loci.txt',
+    genotypesfile                => 'data/genotypes.txt',
+    priorallelefreqfile          => 'data/trueallelefreqs.txt',
+    fixedallelefreqs             => 1,
+    logfile                      => 'log.txt',
+    indadmixturefile             => 'individualVarSamples.txt',
+    indadmixmodefile             => 'posteriormode.txt'
+};
+
+$arg_hash->{resultsdir}         = 'results-11-11';
+#$arg_hash->{admixtureprior}         = "1,1,";
+#$arg_hash->{admixtureprior1}        = "1,1";
+doAnalysis($executable,$arg_hash);
+
+sub getArguments {
+    my $hash = $_[0];
+    my $arg = '';
+    foreach my $key (keys %$hash) {
+	$arg .= ' --'. $key .'='. $hash->{$key};
+    }
+    return $arg;
+}
+
+sub doAnalysis {
+    my ($prog, $args) = @_;
+    my $command = $prog.getArguments($args);
+    if (-e $args->{resultsdir}) {
+	rmtree($args->{resultsdir});
+    } 
+    mkpath($args->{resultsdir});
+    $ENV{'RESULTSDIR'} = $args->{resultsdir};
+    print "Results will be written to subdirectory $ENV{'RESULTSDIR'}";
+    system($command);
+    print "Starting R script to process output\n";
+    system("R CMD BATCH --quiet --no-save --no-restore \
+           c:/cvs/genepi/test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt");
+    print "R script completed\n\n";
+}
+
