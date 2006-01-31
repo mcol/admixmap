e8d5e95762e48725a36191391f45bfb4a430013e
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-01-31 13:16:27 +0000

    fix for NewtonRaphson function

--- admixmap/GaussianProposalMH.cc
+++ admixmap/GaussianProposalMH.cc
@@ -2,21 +2,13 @@
  *   ADMIXMAP
  *   GaussianProposalMH.cc (formerly MetropolisHastings.cc)
  *   This class is used to implement a Metropolis Hastings update with Gaussian proposal distribution
- *   Copyright (c) 2002, 2003, 2004, 2005 LSHTM
+ *   Copyright (c) 2002-2006 David O'Donnell, Clive Hoggart and Paul McKeigue
  *  
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * This program is free software distributed WITHOUT ANY WARRANTY. 
+ * You can redistribute it and/or modify it under the terms of the GNU General Public License, 
+ * version 2 or later, as published by the Free Software Foundation. 
+ * See the file COPYING for details.
  * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #include "GaussianProposalMH.h"
 
@@ -37,36 +29,60 @@ GaussianProposalMH::~GaussianProposalMH()
 
 int GaussianProposalMH::Sample( double *x, const void* const args )
 {
-  newnum = *x;
-  NewtonRaphson(args);//sets ddf, 2nd derivative at mode, used to set precision of proposal distribution
+  //determine mode of target distribution
+  //and set ddf to 2nd derivative at mode
+  double newnum = FindModeByNewtonRaphson(*x, args);
 
   //generate proposal from Normal distribution centred on mode of target distribution with precision 
-  //given as sqrt of 2nd derivative at the mode
+  //given as sqrt of -2nd derivative at the mode
   double xnew = gennor( newnum, 1 / sqrt( -ddf ) );
 
-  double LogTarget = (*function)( *x, args );//log posterior at current value
-  double NewLogTarget = (*function)( xnew, args );//log posterior at proposal
+  double LogTarget = (*function)( *x, args );//log of target dist at current value
+  double NewLogTarget = (*function)( xnew, args );//log of target dist at proposal
 
   double ProposalRatio = LogNormalDensity( xnew, newnum, -ddf ) - LogNormalDensity( *x, newnum, -ddf );
 
   double LogAcceptanceProb = NewLogTarget - LogTarget + ProposalRatio;
-  int flag = 0;
+  int accept = 0;
   if( log( myrand() ) < LogAcceptanceProb ){
     *x = xnew;
-    flag = 1;
+    accept = 1;
   }
-  return flag;//returns indicator variable for acceptance
+
+  return accept;//returns indicator variable for acceptance
 }
 
-void GaussianProposalMH::NewtonRaphson(const void* const args)
+double GaussianProposalMH::FindModeByNewtonRaphson(double init, const void* const args)
 {
-  double step, df;
-  do{
-    ddf = (*ddfunction)( newnum, args );//2nd derivative
-    df = (*dfunction)( newnum, args );//1st derivative
+  //init is an initial guess, args are arguments to loglikelihood and derivative functions
+  //returns mode as estimate of mode of f
+  double step, f, flast;
+  
+  double df = (*dfunction)( init, args );//1st derivative at initial value
+  ddf = (*ddfunction)( init, args );//2nd derivative " " 
+
+  double newnum = init;
+  while( fabs(df) > 0.001 ){
+    flast = (*function)(newnum, args);//loglikelihood at current value
     step = -df / ddf;
-    newnum += step;
-  }while( fabs(df) > 0.001 );
+
+    f = (*function)(step+newnum, args);//loglikelihood at new value
+    if(f > flast)//if NR step increases loglikelihood
+      newnum += step;//use it
+    else{//use simple random walk to find a point nearer the mode
+      //flast = f; 
+      do{
+	step = gennor(newnum, 0.1 / sqrt( -ddf ) );//could improve this, maybe with StepSizeTuner
+	f = (*function)(step, args);
+      }
+      while(f < flast);//repeat until loglikelihood increases
+      newnum = step;
+    }
+
+    df = (*dfunction)( newnum, args );//update 1st derivative
+    ddf = (*ddfunction)( newnum, args );//update 2nd derivative
+  }
+  return newnum;
 }
 
 double GaussianProposalMH::LogNormalDensity(double x, double mu, double lambda)
--- admixmap/GaussianProposalMH.h
+++ admixmap/GaussianProposalMH.h
@@ -3,21 +3,13 @@
  *   ADMIXMAP
  *   GaussianProposalMH.h (formerly MetropolisHastings.h)
  *   This class is used to implement a Metropolis Hastings update with Gaussian proposal distribution
- *   Copyright (c) 2002, 2003, 2004, 2005 LSHTM
+ *   Copyright (c) 2002-2006 David O'Donnell, Clive Hoggart and Paul McKeigue
  *  
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * This program is free software distributed WITHOUT ANY WARRANTY. 
+ * You can redistribute it and/or modify it under the terms of the GNU General Public License, 
+ * version 2 or later, as published by the Free Software Foundation. 
+ * See the file COPYING for details.
  * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #ifndef GAUSSIANPROPOSALMH_H
 #define GAUSSIANPROPOSALMH_H 1
@@ -38,7 +30,6 @@ public:
 
 private: // members
   unsigned dim;
-  double newnum;
   double ddf;
 
   double (*function)( const double, const void* const );
@@ -46,7 +37,7 @@ private: // members
   double (*ddfunction)( const double, const void* const );
 
   // HELPER FUNCTIONS
-  void NewtonRaphson(const void* const args);
+  double FindModeByNewtonRaphson(double init, const void* const args);
   static double LogNormalDensity
   (double x, double mu, double lambda);
 
