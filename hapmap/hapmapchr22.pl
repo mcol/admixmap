20b9f49015da9d9cc592a0a55d45222abe1dc712
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-03-07 00:12:43 +0000

    no message

---
+++ hapmap/hapmapchr22.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+use strict; 
+use File::Path;
+
+sub getArguments
+{
+    my $hash = $_[0];
+    my $arg = '';
+    foreach my $key (keys %$hash){
+	$arg .= ' --'. $key .'='. $hash->{$key};
+    }
+    return $arg;
+}
+
+sub doAnalysis
+{
+    my ($prog,$args) = @_;
+    my $command = $prog.getArguments($args);
+    if (-e $args->{resultsdir}) {
+	rmtree($args->{resultsdir});
+    } 
+    mkpath($args->{resultsdir});
+    $ENV{'RESULTSDIR'} = $args->{resultsdir};
+    print "\nResults will be written to subdirectory $ENV{'RESULTSDIR'}";
+    system($command);
+    print "Starting R script to process output\n";
+    system("RCMD BATCH --quiet --no-save --no-restore c:/cvs/genepi/test/AdmixmapOutput.R \
+            $args->{resultsdir}/Rlog.txt");
+    print "R script completed\n\n";
+}
+
+################### DO NOT EDIT ABOVE THIS LINE ########################
+my $executable = 'c:/cvs/genepi/test/admixmap';
+
+my $arg_hash = {
+#data files
+    genotypesfile                   => 'data/genotypes.txt',
+    locusfile                       => 'data/loci.txt',
+#main options
+    samples  => 70,
+    burnin   => 20,
+    every    => 2,
+    numannealedruns => 0,
+    displaylevel => 3, 
+    populations => 1,
+    sumintensitiesprior => '40,11,10',
+#output file options
+    logfile                     => 'log.txt',
+# optional tests
+    residualallelicassocscorefile => 'residualLDscoretests.txt',
+};
+
+
+# model with 1 block state
+$arg_hash->{resultsdir}            = 'resultschr22_1State';  
+#doAnalysis($executable,$arg_hash);
+
+# model with 2 block states
+$arg_hash->{hapmixmodel}           = 1;
+$arg_hash->{populations}           = 2;
+$arg_hash->{resultsdir}            = 'resultschr22_2State';  
+#doAnalysis($executable,$arg_hash);
+
+# model with 2 block states
+$arg_hash->{populations}           = 4;
+$arg_hash->{resultsdir}            = 'resultschr22_4State';  
+doAnalysis($executable,$arg_hash);
+
+############### DO NOT EDIT BELOW THIS LINE ############################
