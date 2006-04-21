5c3974efc8b33277c6bbd134e678d8fa4055354a
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-04-21 14:50:29 +0000

    fixed extra locus error

--- hapmap/convertdata.cc
+++ hapmap/convertdata.cc
@@ -29,21 +29,22 @@ string getGenotype(string g, string a){
 
 int main(int argc, char **argv){
     if(argc==1){
-	cout << "Usage: convertdata chr [genotypesfile [locusfile]]" << endl
-	     << "where is a chromosome number; chr=0 converts all chromosomes." << endl;
+	cout << "Usage: convertdata c [genotypesfile [locusfile]]" << endl
+	     << "where c is a chromosome number; chr=0 converts all chromosomes." << endl;
 	exit(0);
     }
   if(argc>1) CHRNUM = atoi(argv[1]);
   ifstream genotypesin;
   ifstream infofile("pedinfo2sample_CEU.txt");//custom file with info on individuals' sex and validity (only unrelated inds are valid)
   ofstream genotypesfile;
-  if(argc>2)genotypesfile.open(argv[2]);
-  else genotypesfile.open("genotypes.txt");
+  if(argc>2)genotypesfile.open(argv[2], ios::out);
+  else genotypesfile.open("genotypes.txt", ios::out);
   ofstream locusfile;
   if(argc>3)locusfile.open(argv[3]);
   else locusfile.open("loci.txt");
 
   ofstream badlocusfile("excludedSNPs.txt");
+  //ofstream outcomefile("outcome_halfmissing.txt");
   string alleles;
   string genotype;
   string obs;//observed genotype
@@ -61,7 +62,7 @@ int main(int argc, char **argv){
   if(CHRNUM==0)CHRNUM = 1;
   else lastchr = CHRNUM;
 
-  //NB: assumes individuals in same order in both files
+  //NB: assumes individuals in same order in all files
 
   //read info file for indiv id, sex
   NUMIND = 0;
@@ -159,7 +160,8 @@ int main(int argc, char **argv){
       //write indiv id and sex
       genotypesfile << INDIVID[indiv] << "\t";// << sex[indiv] <<"\t";
       position = 0.0;     
-
+//       if(indiv%2) outcomefile << "#\n";
+//       else outcomefile << 9 << endl;
       for(unsigned chr = CHRNUM; chr <= lastchr; ++chr){
 	  cout << "\nChromosome " << chr << "  "<< flush ;
 	  stringstream ss;
@@ -173,9 +175,9 @@ int main(int argc, char **argv){
       
 	  //read genotypes, one locus (row) at a time
 	  //for(locus = 0; locus < NUMLOCI+ NUMBADLOCI/*BadLoci.size()*/; ++locus){
+	  prev = position;
+	  genotypesin >> SNPID >> alleles >> scrap >> position;
 	  while (! genotypesin.eof() ){
-	      prev = position;
-	      genotypesin >> SNPID >> alleles >> scrap >> position;
 	      if(position-prev >=0.0){//skip loci out of sequence
 		  //cout << "  locus" << locus+1 << " " << SNPID << " " <<position << " " << prev << endl;
 		  for(int col = 0; col < 7+indiv; ++col)genotypesin >> scrap;//skip to col for this individual, genotypes start at col 11
@@ -183,14 +185,16 @@ int main(int argc, char **argv){
 		  
 		  //write to genotypesfile in admixmap format
 		  //set 50% to missing at half the loci
-		  //if(indiv%2 && locus%2)genotypesfile << "\"0,0\"" << "\t";
-		  //else
-		  genotypesfile << getGenotype(obs, alleles) << "\t";
-		  //cout << indiv << " "  << INDIVID[indiv] << " " << locus << " " << SNPID << " " << obs << " " <<  alleles << " " << getGenotype(obs, alleles) << endl << flush;
-		  //if(!(locus%10))system("pause");
+// 		  if(indiv%2 && locus%2){
+// 		      genotypesfile << "\"0,0\"" << "\t";
+// 		  }
+// 		  else{
+ 		      genotypesfile << getGenotype(obs, alleles) << "\t";
+// 		  }
 	      }
-	      //else 	 cout << "  locus" << locus+1 << " " << SNPID << " " <<position << " " << prev << endl;
 	      getline(genotypesin, scrap);//skip remaining individuals in line
+	      prev = position;//start reading next line
+	      genotypesin >> SNPID >> alleles >> scrap >> position;
 	  }
 	  genotypesin.close();
       }
@@ -198,5 +202,6 @@ int main(int argc, char **argv){
       genotypesfile << endl;
     }
   genotypesfile.close();
+//  outcomefile.close();
   cout << "\nFinished writing genotypesfile" << endl;
 }
