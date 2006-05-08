a8a5be41d8dd3ee3af36c7ff7d44557295a82493
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-05-08 13:36:51 +0000

    moved includes from header

--- admixmap/CompositeLocus.h
+++ admixmap/CompositeLocus.h
@@ -3,32 +3,19 @@
  *   ADMIXMAP
  *   CompositeLocus.h 
  *   header file for CompositeLocus class
- *   Copyright (c) 2002, 2003, 2004, 2005 LSHTM
+ *   Copyright (c) 2002-2006 David O'Donnell, Clive Hoggart and Paul McKeigue
  *  
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * This program is free software distributed WITHOUT ANY WARRANTY. 
+ * You can redistribute it and/or modify it under the terms of the GNU General Public License, 
+ * version 2 or later, as published by the Free Software Foundation. 
+ * See the file COPYING for details.
  * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef COMPOSITE_LOCUS_H
 #define COMPOSITE_LOCUS_H 1
 
-#include "rand.h"
 #include "common.h"
-#include <iomanip>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 typedef struct
  {
