34688af592360d0260f198194f3127230f3a2ee1
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-05-10 17:24:24 +0000

    more, smaller steps

--- admixmap/AlleleFreqSampler.cc
+++ admixmap/AlleleFreqSampler.cc
@@ -33,9 +33,9 @@ void AlleleFreqSampler::SampleAlleleFreqs(double *phi, const double* Prior, Indi
   Args.coolness = coolness;
 
   //initialise Hamiltonian Sampler
-  double step0 = 0.005;//initial step size
+  double step0 = 0.001;//initial step size
   double min = -100.0, max = 100.0; //min and max stepsize
-  int numleapfrogsteps = 20;
+  int numleapfrogsteps = 40;
   Sampler.SetDimensions(dim, step0, min, max, numleapfrogsteps, 0.44/*target acceptrate*/, getEnergy, gradient);
 
   //transform phi 
