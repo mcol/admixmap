1c9f1f95710c16ff7de481897953ea7b57ee909e
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-06-28 12:13:13 +0000

    *** empty log message ***

--- test/sim.R
+++ test/sim.R
@@ -25,32 +25,59 @@ simulateGenotypes <- function(M1,M2, rho,x,L) {
   simulateGenotypes <- paste(paternalGamete, ",", maternalGamete, sep="")
 }
 
+distanceFromLast <- function(v.Chr, v.Position) {
+## given a table of loci with chr number and position in cM or megabases,
+## this function returns vector of distances from last locus
+## distance from last coded as NA for first locus on each chromosome 
+  OrderedPosition <- 10000*v.Chr + v.Position
+  v.DistanceFromLast <- c(10000,
+                          (OrderedPosition[-1]) - OrderedPosition[-length(OrderedPosition)])
+  v.DistanceFromLast[v.DistanceFromLast > 1000] <- NA
+  return(v.DistanceFromLast)
+}
+
 ##########################################################################
 ## Start of script
 K <- 2
-N <- 500
+N <- 200
 rho <- 6 ## sum-of-intensities
+spacing <- 40 # 40 cM spacing gives 99 loci
 L <- 50 #default # number of loci
 beta <- 2 # regression slope
+popadmixparams <- c(3, 1) # population admixture params for pop1, pop2
+afreqparams <- c(10, 90)
 
 ##marker spacings#
 # x <- 0.1 #evenly spaced#
 #x <- runif(L,0.01,0.05) #default, spacings between 1 and 5 cM
-x <- rep(NA,L) #unlinked markers
+#x <- rep(NA,L) #unlinked markers
 # x <- rep(0.01,L) #denser markers
+
+x <- numeric(0)
+chr <- numeric(0)
+numChr <- 22
+## chromosome lengths in cM
+chr.L <- c(292,272,233,212,197,201,184,166,166,181,156,169,117,128,110,130,128,123,109,96,59,58)
+length <- sum(chr.L)
+for(chromosome in 1:22) {
+  positions <- seq(0, chr.L[chromosome], spacing)
+  x <- c( x, positions) 
+  chr <- c(chr, rep(chromosome, length(positions)))
+}
+x <- 0.01*distanceFromLast(chr, x)
+L <- length(x) # number of loci
 
 ## simulate allele freqs
 alleleFreqs <- matrix(data=NA, nrow=2*L, ncol=K)
 for(locus in 1:L) {
-  alleleFreqs[2*locus - 1, 1] <- rbeta(1, 20, 80) # freqs allele 1 in subpop 1
-  alleleFreqs[2*locus - 1, 2] <- rbeta(1, 80, 20) # freqs allele 1 in subpop 2
+  alleleFreqs[2*locus - 1, 1] <- rbeta(1, afreqparams[1], afreqparams[2]) # freqs allele 1 in subpop 1
+  alleleFreqs[2*locus - 1, 2] <- rbeta(1, afreqparams[2], afreqparams[1]) # freqs allele 1 in subpop 2
   alleleFreqs[2*locus, ] <- 1 - alleleFreqs[2*locus - 1, ] # freqs allele 2
 }
 
 genotypes <- character(L)
 outcome <- numeric(N)
 avM <- numeric(N)
-popadmixparams <- c(3, 1) # population admixture params for pop1, pop2
 popM <- popadmixparams[2] / sum(popadmixparams) # mean admixture proportions
 for(individual in 1:N) {
   M1 <- rbeta(1, popadmixparams[1], popadmixparams[2])
@@ -86,7 +113,8 @@ dimnames(loci)[[2]] <- c("Locus", "NumAlleles", "Distance")
 write.table(loci, file="simdata/loci.txt", row.names=FALSE, sep="\t")
 
 ## write priorallelefreqs file
-priorallelefreqs <- t(matrix(c(20,80,80,20), ncol=2*L, nrow=2))
+priorallelefreqs <- t(matrix(c(afreqparams[1], afreqparams[2],
+                               afreqparams[2], afreqparams[1]), ncol=2*L, nrow=2))
 locusnames <- rep(loci[, 1], each=2)
 priorallelefreqs <- data.frame(locusnames, priorallelefreqs)
 dimnames(priorallelefreqs)[[2]] <- c("Locus", "Pop1", "Pop2")
