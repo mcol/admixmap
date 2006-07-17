df73d8cf948e03271fd7e748610e880c7e594122
lshtm <lshtm@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-07-17 00:06:32 +0000

    made some functions private, changed name of GetOnePopOneIndLogLikelihood, added UpdateIndivAdmixtureRandomWalk

--- admixmap/IndividualCollection.h
+++ admixmap/IndividualCollection.h
@@ -43,19 +43,24 @@ public:
   IndividualCollection(const AdmixOptions* const options, const InputData* const Data, Genome* Loci);
   void SetNullValues();
   void DeleteGenotypes(bool);
-
   void Initialise(const AdmixOptions* const options, const Genome* const Loci, const string* const PopulationLabels,
 		  const std::vector<std::vector<double> > &alpha, //double rhoalpha, double rhobeta, 
 		  LogWriter &Log);
-
   void LoadData(const AdmixOptions* const options, const InputData* const);
-  
-  void getOnePopOneIndLogLikelihood(LogWriter &Log, const std::string* const PopulationLabels);
+  void getOneIndLogLikelihood(LogWriter &Log, const AdmixOptions* const options, 
+			      const std::string* const PopulationLabels);
 
   void SampleLocusAncestry(int iteration, const AdmixOptions* const options,
-			   const vector<Regression*> &R, const double* const poptheta,
-			   const vector<vector<double> > &alpha, 
-			   bool anneal);
+				      const vector<Regression*> &R, const double* const poptheta,
+				      const vector<vector<double> > &alpha, 
+				      bool anneal);
+  void UpdateIndivAdmixtureRandomWalk(int iteration, const AdmixOptions* const options,
+				      const vector<Regression*> &R, const double* const poptheta,
+				      const vector<vector<double> > &alpha, 
+				      bool anneal);
+  void SampleLocusAncestry(int iteration, const AdmixOptions* const options,
+			   const vector<Regression*> &R);
+  
   void SampleHapPairs(const AdmixOptions* const options, AlleleFreqs *A, const Genome* const Loci,
 		      bool anneal);
   void SampleParameters(int iteration, const AdmixOptions* const options,
@@ -79,28 +84,17 @@ public:
 			);
 
   void annealGenotypeProbs(unsigned nchr, const double coolness, const double* Coolnesses);
-  
   void OutputIndAdmixture();
-
   double getDevianceAtPosteriorMean(const AdmixOptions* const options, vector<Regression *>&R, Genome* Loci, LogWriter &Log, 
 				    const vector<double>& SumRho, unsigned numChromosomes, AlleleFreqs* A);
-
-  //void OutputChibEstimates(bool, LogWriter &, int)const;
   void OutputChibResults(LogWriter&)const;
-
-  //void OutputResiduals(const char* ResidualFilename, const Vector_s Labels, int iterations);
   void OpenExpectedYFile(const char* Filename, LogWriter & Log);
   void OutputExpectedY(int k);
   void FinishWritingEYAsRObject(unsigned NumIterations, const Vector_s Labels);
-
   int getSize()const;
   void add(Individual*);
-
   Individual* getIndividual(int)const;
-
-  void setAdmixtureProps(const double* const, size_t);
-  //void setAdmixturePropsX(const double* const, size_t);
-
+  //void setAdmixtureProps(const double* const, size_t);
   double GetSumrho()const;
   double getSumLogTheta(int)const;
   const double *getSumLogTheta()const;
@@ -111,7 +105,6 @@ public:
   unsigned GetSNPAlleleCounts(unsigned locus, int allele)const;
   int getNumberOfMissingGenotypes(unsigned locus)const;
   const vector<int> getAlleleCounts(unsigned locus, int pop, unsigned NumStates)const;
-
   std::vector<double> getOutcome(int)const;
   double getOutcome(int, int)const;
     bool isMissingOutcome(int, int)const;
@@ -121,29 +114,26 @@ public:
   const double* getCovariates()const;
   DataType getOutcomeType(int)const;
   void SetExpectedY(int, const double*const );
-  //void UpdateSumResiduals();
   double getExpectedY(int)const;
   double getExpectedY(int, int)const;
   const std::string getCovariateLabels(int)const;
   const Vector_s getCovariateLabels()const;
   double getSampleVarianceOfOutcome(int j)const;
   double getSampleVarianceOfCovariate(int j)const;
-
   double getEnergy(const AdmixOptions* const options, const vector<Regression*> &R, 
 			  const bool & annealed);
   void accumulateEnergyArrays(const AdmixOptions* const options);
   double* getSumEnergy();
   double* getSumEnergySq();
-
   double DerivativeInverseLinkFunction(int i)const;
   void ResetChib();
   void OutputErgodicChib(std::ofstream *avgstream, bool fixedfreqs);
   const chib* getChib()const;
-
   void HMMIsBad(bool b);
   void resetStepSizeApproximators(int k); 
 
 private:
+  int Populations;
   Individual **_child; //array of pointers to Individual objects
   Individual** TestInd;// pointer to individual for whom to estimate marginal likelihood
   int sizeTestInd;
@@ -154,25 +144,17 @@ private:
   void LoadCovariates(const InputData*, const AdmixOptions* const options);
   void LoadOutcomeVar(const InputData* const);
   void LoadRepAncestry(const InputData* const);
-  //void InitialiseMLEs(double, double, const AdmixOptions* const);
 
   unsigned int NumInd, size, NumCompLoci;
-  //MLEs of Individual admixture and sumintensities
-  //used to calculate marginal likelihood
-  //vector<double> rhohat;
-  //double *thetahat;
   std::vector< std::vector<double> > admixtureprior;
-
   double *SumLogTheta;//sums of log individual admixture proportions
-  //vector<double> MaxLogLikelihood;
-
   DataMatrix *ReportedAncestry;
-  //std::vector<double> sigma;
   IndAdmixOutputter* indadmixoutput;
   double SumLogLikelihood;
   double SumDeviance, SumDevianceSq;
   std::vector< int > _locusfortest;
   int* SumAncestry;//for update of locus-specific sumintensities
+
 #ifdef PARALLEL
   int* GlobalSumAncestry;//SumAncestry summed over processes, kept on master processes
   int rank_with_freqs;
@@ -183,7 +165,6 @@ private:
  
   //Regression Objects
   double **ExpectedY;
-  //double **SumResiduals;
   DataMatrix Outcome;
   int NumOutcomes;
   int NumCovariates;//covariates including admixture
@@ -192,7 +173,6 @@ private:
   Vector_s CovariateLabels;
   DataType *OutcomeType;
   std::ofstream EYStream;//output file for expected outcome
-
   chib MargLikelihood;
 };
 
