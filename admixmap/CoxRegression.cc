1a9ffcd1cbe69ed021954df5421f6f4f382c5bf2
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-07-27 15:37:28 +0000

    moved ExpectedY from Indid=vidualCollection to Regression

--- admixmap/CoxRegression.cc
+++ admixmap/CoxRegression.cc
@@ -2,6 +2,7 @@
 #include <algorithm>
 
 using namespace::std;
+const double* CoxRegression::EY;
 
 CoxRegression::CoxRegression(){
   BetaSampler = 0;
@@ -19,19 +20,29 @@ CoxRegression::~CoxRegression(){
 }
 
 void CoxRegression::Initialise(unsigned Number, double priorPrecision, const IndividualCollection* const individuals, LogWriter &Log){
-  Regression::Initialise(Number, individuals->GetNumCovariates(), individuals->getSize(), individuals->getCovariates());
+  Regression::Initialise(Number, individuals->GetNumCovariates()-1, individuals->getSize(), individuals->getCovariates());
+  //passing numcovariates-1 to exclude intercept
+  // NumCovariates in this class is now one less
 
   //TODO: ?? rescale precisions somehow 
-  betaprecision[0] = priorPrecision;
   Log.setDisplayMode(Quiet);
-  Log << "\nGaussian priors on Cox regression parameters with zero means and precisions\n ("<< betaprecision[0];
+  Log << "\nGaussian priors on Cox regression parameters with zero means and precisions\n (";
   
-  for(int j = 1; j < NumCovariates; ++j){
+  for(int j = 0; j < NumCovariates; ++j){
     betaprecision[j] = priorPrecision ;//* individuals->getSampleVarianceOfCovariate(j) ;
     Log << ", " << betaprecision[j];
   }
   Log << ")\n";
-  
+
+  //SetExpectedY(Covariates, beta, ExpectedY);  
+  for(int i = 0; i < NumIndividuals; ++i){
+    ExpectedY[i] = 0.0;
+    for(int j = 0; j < NumCovariates; ++j)
+      ExpectedY[i] += Covariates[i*(NumCovariates+1)+j+1]*beta[j]; 
+    ExpectedY[i] = myexp(ExpectedY[i]);
+  }
+  EY = ExpectedY;
+
   //initialisation specific to Cox Regression
   BetaParameters.c = 0.01;//TODO: decide sensible defaults for these, add options to allow user to set values
   BetaParameters.mu = 1;
@@ -101,11 +112,17 @@ void CoxRegression::Update(bool sumbeta, const std::vector<double>& , const doub
   BetaParameters.coolness = coolness; 
   
   try{
-    for( int j = 1; j < NumCovariates; j++ ){
+    for( int j = 0; j < NumCovariates; j++ ){
+      double oldbeta = beta[j];
       BetaParameters.beta0 = betamean[j];
       BetaParameters.priorprecision = betaprecision[j];
       BetaParameters.index = j;
       acceptbeta = BetaSampler->Sample( beta + j, &BetaParameters );
+      if(acceptbeta){
+	for(int i = 0; i < NumIndividuals; ++i){
+	  ExpectedY[i] *= myexp(Covariates[i*(NumCovariates+1)+j+1] * (beta[j] - oldbeta)) ;  
+	}
+      }
     }
   }
   catch(string s){
@@ -128,8 +145,17 @@ void CoxRegression::Update(bool sumbeta, const std::vector<double>& , const doub
     SumParameters();
     //TODO:accumulate sums of hazard rates
   }
-  
 }
+
+//void CoxRegression::SetExpectedY(const double* const Covariates, const double* const beta, double* ExpectedY){
+  //doesn't really set expected outcome. stores Xbeta for use in updates
+//   for(unsigned i = 0; i < NumIndividuals; ++i){
+//     ExpectedY[i] = 0.0;
+//     for(unsigned j = 0; j < NumCovariates; ++j)
+//       ExpectedY[i] += Covariates[i*(NumCovariates+1)+j+1]*beta[j]; 
+//     ExpectedY[i] = myexp(ExpectedY[i]);
+//   }
+//}
 
 void CoxRegression::OutputParams(ostream* out){
   Regression::OutputParams(out);
@@ -143,6 +169,25 @@ double CoxRegression::getDispersion()const{
 //TODO
 double CoxRegression::DerivativeInverseLinkFunction(unsigned)const{
   return 1.0;    
+}
+
+///given an array of regression parameters beta and covariates X, computes expected outcome EY = X * beta, 
+///with index'th element of beta replaced with betaj.
+void CoxRegression::getExpectedOutcome(const double* const beta, const double* const X, double* EY, int n, int dim, int index, double betaj){
+  //NOTE: here, X has dimension n* (dim+1 ) as it includes the intercept
+  for(int i = 0; i < n; ++i){
+    EY[i] = 0.0;
+    for(int j = 0; j < dim; ++j)
+      if(j == index)
+	EY[i] += X[i*(dim+1)+j+1]*betaj;
+      else
+	EY[i] += X[i*(dim+1)+j+1]*beta[j];
+  }
+}
+
+///given an array of regression parameters beta and covariates X, computes expected outcome EY = X * beta
+void CoxRegression::getExpectedOutcome(const double* const beta, const double* const X, double* EY, int n, int d){
+  getExpectedOutcome(beta, X, EY, n, d, -1, 0.0);
 }
 
 double CoxRegression::lr(const double beta, const void* const vargs){
@@ -161,24 +206,26 @@ double CoxRegression::lr(const double beta, const void* const vargs){
 
   //log likelihood
   if(args->coolness > 0.0){
-    double *Xbeta = new double[ n ];    
     try{
-      Regression::getExpectedOutcome(args->beta, args->Covariates, Xbeta, n, args->d, index, beta);
       for(int i = 0; i < n; ++i){
-	double exp_Xbetai = exp(Xbeta[i]);
+	double exp_Xbetai = EY[i] * myexp(args->Covariates[i*(args->d+1)+index+1] * (beta - args->beta[index])) ;  
+	int sum_nr = 0;
+	double sum_nrlogalpha = 0.0, sumalpha = 0.0;
+
 	for(int t = 0; t < T; ++t){
 	  if( atRisk(firstStart+i, firstEnd+i, firstEndpoint+t) ){
-	    const double lambda = *(hazardRates + t)*exp_Xbetai;
 	    int r = *(args->events +i);
-	    f += (double)r * mylog(lambda) - lambda; 
+	    sum_nr += r;
+	    sum_nrlogalpha += r*mylog(*(hazardRates + t));
+	    sumalpha += *(hazardRates + t);
 	  }
 	}
+	f += sum_nrlogalpha + mylog(exp_Xbetai)*(double)sum_nr  - exp_Xbetai * sumalpha; 
       }
     }
     catch(string s){
       throw string("error in lr: " +s );
     }
-    delete[] Xbeta;
   }
   //log prior on beta
   f -= 0.5 * args->priorprecision * (beta - beta0) * (beta - beta0);
@@ -187,6 +234,7 @@ double CoxRegression::lr(const double beta, const void* const vargs){
   //     f += (args->mu * (*(firstEndpoint+t+1) - *(firstEndpoint+t)) * c - 1.0)*mylog(*(hazardRates +t)) - c *  *(hazardRates+t);
   //   }
   //TODO: loops in loglikelihood could be rearranged to include log prior on hazard rates
+  //if(index==2)cout << "f = " << f << endl; 
   return f;
 }
 
@@ -206,35 +254,34 @@ double CoxRegression::dlr(const double beta, const void* const vargs){
 
   //log likelihood
   if(args->coolness > 0.0){
-    double *Xbeta = new double[ n ];    
     try{
-      Regression::getExpectedOutcome(args->beta, args->Covariates, Xbeta, n, args->d, index, beta);
       
       for(int i = 0; i < n; ++i){
 	int sum_nr = 0;
-	double sum_nlambda = 0.0;
-	double exp_Xbetai = exp(Xbeta[i]);
+	double sum_nalpha = 0.0;
+	double exp_Xbetai = EY[i] * myexp(args->Covariates[i*(args->d+1)+index+1] * (beta - args->beta[index])) ;  
+	  //exp(Xbeta[i]);
+
 	for(int t = 0; t < T; ++t){
 	  if( atRisk(firstStart+i, firstEnd+i, firstEndpoint+t) ){
-	    //cout << "lambda" << i << t << " = " << lambda << " r= " << r << endl;
 	    sum_nr += *(args->events +i);
-	    sum_nlambda += *(args->HazardRates + t); 
+	    sum_nalpha += *(args->HazardRates + t); 
 	  }
 	}
 	//cout << "sum " << i << " = " << sum << endl;
-	f += ((double)sum_nr - exp_Xbetai * sum_nlambda) * args->Covariates[i*args->d +index];
+	f += ((double)sum_nr - exp_Xbetai * sum_nalpha) * args->Covariates[i*(args->d+1) +index+1];
       }
     }
     catch(string s){
       throw string("error in dlr: " +s );
     }
 
-    delete[] Xbeta;
   }
   f -= args->priorprecision * (beta - beta0);//log prior contribution
 //   for(int t = 0; t < T; ++t){
 //     f += (args->mu * (*(firstEndpoint+t+1) - *(firstEndpoint+t)) * c - 1.0) / (*(hazardRates +t)) - c ;
 //   }
+  //if(index==2)cout << "df = " << f << endl; 
   return f;
 }
 
@@ -253,19 +300,21 @@ double CoxRegression::ddlr(const double beta, const void* const vargs){
 
   //log likelihood
   if(args->coolness > 0.0){
-    double *Xbeta = new double[ n ]; 
+    //double *Xbeta = new double[ n ]; 
     try{
-      Regression::getExpectedOutcome(args->beta, args->Covariates, Xbeta, n, args->d, index, beta);
+      //getExpectedOutcome(args->beta, args->Covariates, Xbeta, n, args->d, index, beta);
       
       for(int i = 0; i < n; ++i){
 	double sum = 0.0;
-	double exp_Xbetai = exp(Xbeta[i]);
+	double exp_Xbetai = EY[i] * myexp(args->Covariates[i*(args->d+1)+index+1] * (beta - args->beta[index])) ;  
+	  //exp(Xbeta[i]);
+
 	for(int t = 0; t < T; ++t){
 	  if( atRisk(firstStart+i, firstEnd+i, firstEndpoint+t) ){
 	    sum += *(args->HazardRates + t); 
 	  }
 	}
-	double x = args->Covariates[i*args->d +index];
+	double x = args->Covariates[i*(args->d+1) +index+1];
 	f -= sum * x * x *exp_Xbetai;
       }
     }
@@ -273,12 +322,13 @@ double CoxRegression::ddlr(const double beta, const void* const vargs){
       throw string("error in ddlr: " +s );
     }
 
-    delete[] Xbeta;
+    //delete[] Xbeta;
   }
   f -= args->priorprecision;//log prior contribution
 //   for(int t = 0; t < T; ++t){
 //     f += (args->mu * (1.0 - *(firstEndpoint+t+1) - *(firstEndpoint+t)) * c ) / ( (*(hazardRates +t)) * (*(hazardRates +t)) );
 //   }
+  //if(index==2)cout << "ddf = " << f << endl; 
   return f;
 }
 
@@ -320,4 +370,8 @@ double CoxRegression::getLogLikelihoodAtPosteriorMeans(int iterations, const std
 
 
   return logL;
+}
+
+void CoxRegression::OutputExpectedY(){
+
 }
