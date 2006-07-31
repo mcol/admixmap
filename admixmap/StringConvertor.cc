8505faea93c4a6d849f7ffd7cf9e59e1a83b7b81
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-07-31 11:39:38 +0000

    replaced CstrToVec with StringToVec

--- admixmap/StringConvertor.cc
+++ admixmap/StringConvertor.cc
@@ -1,8 +1,8 @@
 #include "StringConvertor.h"
-#include "StringSplitter.h"
 
 using namespace std;
 
+/// removes quotes from strings. 
 string StringConvertor::dequote(const string& str)
 {
   if (str.size() >= 2 && (str[0] == '"' || str[0]== '\'') && (str[str.size() - 1] == '"' || (str[str.size() - 1] == '\''))) {
@@ -12,107 +12,36 @@ string StringConvertor::dequote(const string& str)
     return str;
 }
 
+/// converts a string to int
 int StringConvertor::toInt(const string& str)
 {
     return atoi(dequote(str).c_str());
 }
-
+  /// converts a string to float
 double StringConvertor::toFloat(const string& str)
 {
     return atof(dequote(str).c_str());
 }
 
-//pair<int, int> StringConvertor::toIntPair(const string& str)
-// void StringConvertor::toIntPair(unsigned int *a,const string& str)
-// {
-//   cout<<"a= "<<a[0]<<" "<<a[1]<<endl;
-
-//     const std::string tmp = dequote(str);
-//     cout<<"tmp = "<<tmp<<endl;
-//     a[0]=a[1]=0;
-//     // Try to find comma first
-//     string::const_iterator sci = find(tmp.begin(), tmp.end(), ',');
-                    
-//     // ... then try find slash.
-//     if (sci == tmp.end()) sci = find(tmp.begin(), tmp.end(), '/');
-
-//     // If nothing is found return default value.
-//     if (sci == tmp.end()) {//return make_pair(0, 0);
-//       a[0] = a[1] = 0;
-//     }
-//     else{
-//       //cout<<tmp.begin()<<" "<<sci<<" "<<tmp.end()<<endl;
-//       const string allele0_str(tmp.begin(), sci);
-//       const string allele1_str(sci + 1, tmp.end());
-//       //return make_pair(atoi(allele0_str.c_str()), atoi(allele1_str.c_str()));
- 
-//       a[0] = atoi(allele0_str.c_str());
-//       a[1] = atoi(allele1_str.c_str());
-//     }
-//   cout<<"a= "<<a[0]<<" "<<a[1]<<endl;
-//     //return a;
-// }
-
-void StringConvertor::toIntPair(unsigned int *a,const string& str)
+/** 
+    Converts a string to a vector of up to unsigned short ints, splitting on separator sep.
+    Works like Tokenize but splits into only two substrings. 
+*/
+void StringConvertor::toIntPair(std::vector<unsigned short>* a, const std::string& str, const char* sep)
 {
-
-    const std::string tmp = dequote(str);
-
-    a[0]=a[1]=0;
-  
-    int i = tmp.find_first_of(",/"); 
-    if( tmp.find_first_of(",/") < tmp.length()){
-      a[0] = atoi(tmp.substr(0,i).c_str());
-      a[1] = atoi(tmp.substr(i+1,tmp.length()-i).c_str());
-    }
-    else{
-      a[0] = a[1] = 0;
-    }
-                    
+  //strip quotes from string
+  const std::string tmp = dequote(str);
+  //look for a separator
+  string::size_type i = tmp.find_first_of(sep);
+  //extract part of string up to first instance of separator
+  a->push_back(atoi(tmp.substr(0,i).c_str()));
+  if( i != string::npos){//separator found
+    //extract as int the part of the string after separator
+    a->push_back(atoi(tmp.substr(i+1,tmp.length()-i).c_str()));
+  }                      
 }
 
-void StringConvertor::toIntPair(unsigned short *a,const string& str)
-{
-
-    const std::string tmp = dequote(str);
-
-    a[0]=a[1]=0;
-  
-    int i = tmp.find_first_of(",/"); 
-    if( tmp.find_first_of(",/") < tmp.length()){
-      a[0] = atoi(tmp.substr(0,i).c_str());
-      a[1] = atoi(tmp.substr(i+1,tmp.length()-i).c_str());
-    }
-    else{
-      a[0] = a[1] = 0;
-    }
-                    
-}
-void StringConvertor::toIntPair(std::vector<unsigned short> *a,const string& str)
-{
-
-    const std::string tmp = dequote(str);
-    if(a->size() != 0) a->resize(2);
-    (*a)[0]=(*a)[1]=0;
-  
-    int i = tmp.find_first_of(",/"); 
-    if( tmp.find_first_of(",/") < tmp.length()){
-      (*a)[0] = atoi(tmp.substr(0,i).c_str());
-      (*a)[1] = atoi(tmp.substr(i+1,tmp.length()-i).c_str());
-    }
-    else{
-      (*a)[0] = (*a)[1] = 0;
-    }
-                    
-}
-
-bool StringConvertor::isMissingValue(const string& str)
-{
-    static string MISSING[] = {"#", ".", "NA"};
-    return MISSING + 3 != find(MISSING, MISSING + 3, str);
-}
-
-//determines if a char array consists only of whitespace
+/// determines if a char array consists only of whitespace.
 bool StringConvertor::isWhiteLine(const char *p)
 {
     while (*p) {
@@ -124,9 +53,23 @@ bool StringConvertor::isWhiteLine(const char *p)
     return true;
 }
 
-void StringConvertor::CstrToVec(const char* s, std::vector<double>& vec)
+/** determines if a string is a missing value.
+    Missing values are denoted by #, ., or NA.
+*/
+bool StringConvertor::isMissingValue(const std::string& str)
 {
-  string str(s);
+    static string MISSING[] = {"#", ".", "NA"};
+    return MISSING + 3 != find(MISSING, MISSING + 3, str);
+}
+
+/**
+   Converts a string to a vector of floats. 
+   Opening and closing quotes are removed and vector elements are separated by 
+   commas or spaces.
+*/
+void StringConvertor::StringToVec(const string s, std::vector<float>& vec)
+{
+  string str = s;
   string::size_type start = str.find_first_not_of("\" "), size = 0;
   str = str.substr(start, str.find_first_of("\"", start)-1);
   start = 0;
@@ -141,9 +84,13 @@ void StringConvertor::CstrToVec(const char* s, std::vector<double>& vec)
   }
 }
 
-void StringConvertor::CstrToVec(const char* s, std::vector<float>& vec)
+/**
+   Converts a atring to a vector of floats. 
+   As previous function but converts to doubles rather than floats.
+*/
+void StringConvertor::StringToVec(const string s, std::vector<double>& vec)
 {
-  string str(s);
+  string str = s;
   string::size_type start = str.find_first_not_of("\" "), size = 0;
   str = str.substr(start, str.find_first_of("\"", start)-1);
   start = 0;
--- admixmap/StringConvertor.h
+++ admixmap/StringConvertor.h
@@ -14,23 +14,18 @@ public:
   /// converts a string to float
   static double toFloat(const std::string& str);
   
-  //static std::pair<int, int> toIntPair(const std::string& str);
-  /// converts a string to two unsigned ints. 
-  static void toIntPair(unsigned int *,const std::string& str);
-  /// converts a string to two unsigned short ints. 
-  static void toIntPair(unsigned short *,const std::string& str);
   /// converts a string to a vector of two unsigned short ints. 
-  static void toIntPair(std::vector<unsigned short>*, const std::string& str);
+  static void toIntPair(std::vector<unsigned short>*, const std::string& str, const char* sep);
   /// determines if a string is a missing value.
   static bool isMissingValue(const std::string& str);
   /// determines if a char array consists only of whitespace.
   static bool isWhiteLine(const char *p);
   /// removes quotes from strings. 
   static std::string dequote(const std::string& str);
-  /// Converts a C-style char array to a vector of doubles. 
-  static void CstrToVec(const char* str, std::vector<double>&);
-  /// Converts a C-style char array to a vector of floats. 
-  static void CstrToVec(const char* str, std::vector<float>&);
+  /// Converts a string to a vector of doubles. 
+  static void StringToVec(const std::string str, std::vector<double>&);
+  /// Converts a string to a vector of floats. 
+  static void StringToVec(const std::string str, std::vector<float>&);
 
   /// a dummy function
   static bool dummy();
