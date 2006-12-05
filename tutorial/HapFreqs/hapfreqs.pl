1bd3d725b65144c957b218c85483c53dea4fb11a
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2006-12-05 18:08:38 +0000

    updated

--- tutorial/HapFreqs/hapfreqs.pl
+++ tutorial/HapFreqs/hapfreqs.pl
@@ -3,7 +3,7 @@ use strict;
 use File::Path;
 
 # Change this to the location of the admixmap executable
-my $executable = '../../test/admixmap.exe';
+my $executable = '../../test/admixmap';
 
 # $arg_hash is a hash of parameters passed to
 # the executable as arguments.
@@ -15,18 +15,17 @@ my $arg_hash =
     samples                    => 2100, 
     burnin                     => 100,
     every                      => 1,
-    coutindicator              => 0,
+    displaylevel              => 0,
     locusfile                  => 'lociDRD2.txt',
     genotypesfile              => 'genotypesDRD2NAm.txt',
     populations                => 1,
-	analysistypeindicator  => 1, 
     
 # output files
     logfile                    => 'logfile.txt',
     hwscoretestfile            => 'HWtest.txt',
     resultsdir                 => 'DRD2freqresultsNAm',
     allelefreqoutputfile       => 'hapfreq.txt'
-    };
+};
 
 doAnalysis($executable,$arg_hash);
 
@@ -51,26 +50,26 @@ doAnalysis($executable,$arg_hash);
 sub getArguments
 {
     my $hash = $_[0];
-    my $arg = '';
+    my $filename = 'perlargs.txt';
+    open(OPTIONFILE, ">$filename") or die ("Could not open args file");
     foreach my $key (keys %$hash){
-	$arg .= ' --'. $key .'='. $hash->{$key};
+      print OPTIONFILE $key . '=' . $hash->{$key} . "\n";
     }
-    return $arg;
+    close OPTIONFILE;
+    return " ".$filename;
 }
 
 sub doAnalysis
 {
     my ($prog,$args) = @_;
     my $command = $prog.getArguments($args);
-    if (-e $args->{resultsdir}) {
-	rmtree($args->{resultsdir});
-    } 
-    mkpath($args->{resultsdir});
+
     $ENV{'RESULTSDIR'} = $args->{resultsdir};
     print "Results will be written to subdirectory $ENV{'RESULTSDIR'}\n";
-    system($command);
-    print "Starting R script to process output\n";
-    system("RCMD BATCH --quiet --no-save --no-restore ..\\..\\test\\AdmixmapOutput.R $args->{resultsdir}/Rlog.txt");
-    print "R script completed\n\n";
+    if(system($command)){
+      print "Starting R script to process output\n";
+      system("R CMD BATCH --	quiet --no-save --no-restore ../../test/AdmixmapOutput.R $args->{resultsdir}/Rlog.txt");
+      print "R script completed\n\n";
+    }
 }
 
