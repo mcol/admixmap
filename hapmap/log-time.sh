2efe949c0630f0358da6ac01a2f31937ac79f677
maciej <maciej@0c12335c-6928-0410-89ea-c6d2b26f428b>
2007-01-26 18:18:17 +0000

    If no diploid individuals, a log line looks different.

--- hapmap/log-time.sh
+++ hapmap/log-time.sh
@@ -7,13 +7,25 @@
 # Given argument: File with hapmixmap output.
 
 LOG_FILE="$1"
+
+if [ ! -r "$LOG_FILE" ]
+then
+	echo "Can't read $LOGFILE"
+	exit 1
+fi
+
 TIME_FILE=time-stats.txt
 
 POPULATION=$(cat $LOG_FILE | grep Loading | head -n 1 | cut -d" " -f2 | cut -d"/" -f1)
 STATES=$(cat $LOG_FILE | grep states | cut -d" " -f 5)
 SAMPLES=$(cat $LOG_FILE | grep iterations | cut -d" " -f1)
-HAPLOID=$(cat $LOG_FILE | grep haploid | cut -d" " -f4)
-DIPLOID=$(cat $LOG_FILE | grep diploid | cut -d" " -f1)
+HAPLOID=$(cat $LOG_FILE | grep "haploid individuals" | cut -d" " -f4)
+if [ -z "$HAPLOID" ]
+then
+	HAPLOID=$(cat $LOG_FILE | grep "haploid individuals" | cut -d" " -f1)
+fi
+DIPLOID=$(cat $LOG_FILE | grep "diploid"| cut -d" " -f1)
 ELAPSED=$(cat $LOG_FILE | grep "Elapsed seconds" | cut -d"=" -f2)
 
 echo -e >> "$TIME_FILE" "$POPULATION\t$STATES\t$SAMPLES\t$HAPLOID\t$DIPLOID\t$ELAPSED"
+# echo -e "$POPULATION\t$STATES\t$SAMPLES\t$HAPLOID\t$DIPLOID\t$ELAPSED"
