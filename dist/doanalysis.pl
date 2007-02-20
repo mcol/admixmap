67f2fca4a99c8d2f4996aad72b623f01a1ffbffa
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2007-02-20 17:40:03 +0000

    added extra functions to doanalysis for parallel execution

--- dist/doanalysis.pl
+++ dist/doanalysis.pl
@@ -16,22 +16,46 @@ sub getArguments {
 
 sub doAnalysis {
     my ($prog, $rscript, $args) = @_;
-    $ENV{'RESULTSDIR'} = $args->{resultsdir};
-    print "\nResults will be written to subdirectory $ENV{'RESULTSDIR'}\n";
-    my $rcmd = "R CMD";
+    print "\nResults will be written to subdirectory $args->{resultsdir}\n";
+
     if($^O eq "MSWin32") {
-	$rcmd = "Rcmd";
 	$prog =~ s/\//\\/g;
 	if( !($prog =~ m/\.exe/) ) {
-	    $prog = "$prog.exe";
+	    $prog = "$prog.exe";##check: should run ok without .exe extension
 	}
-	$rscript =~ s/\//\\/g;
     }
     if( !(-e $prog) ) {die "$prog not found\n"}; 
+
+    runProgram($prog, $args);
+    runRScript($rscript, $args);
+}
+
+sub doParallelAnalysis {
+    my ($prog, $rscript, $args) = @_;
+    if( !(-e $prog) ) {die "$prog not found\n"}; 
+    $ENV{'RESULTSDIR'} = $args->{resultsdir};
+    runProgram("mpiexec $prog", $args);
+    runRScript($rscript, $args);
+}
+
+sub runProgram {
+    my ($prog, $args) = @_;
+    system $prog.getArguments($args);
+}
+
+sub runRScript {
+    my ($rscript, $args) = @_;
+    my $rcmd = "R CMD";
+    if($^O eq "MSWin32") {
+	$rcmd = "Rcmd";##check: R CMD works on Windows too
+	$rscript =~ s/\//\\/g;
+    }
+
     if( !(-e $rscript) ) {die "$rscript not found\n"}; 
     my $rcmdArgs = "BATCH --quiet --no-save --no-restore";  
-    system $prog.getArguments($args);
+
     print "Starting R script to process output\n";
+    $ENV{'RESULTSDIR'} = $args->{resultsdir};
     system("$rcmd $rcmdArgs $rscript $args->{resultsdir}/Rlog.txt\n");
     print "R script completed\n\n";
 }
