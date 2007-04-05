6650c975531785518d3fbce38d047017da8695a7
maciej <maciej@0c12335c-6928-0410-89ea-c6d2b26f428b>
2007-04-05 09:23:33 +0000

    Parametrized separator for generating tables for Trac wiki.

--- hapmap/collect-mi.sh
+++ hapmap/collect-mi.sh
@@ -4,22 +4,32 @@
 
 source config.sh
 
+SUFFIX="$1"
+SEP="||"
+INT_SEP=" $SEP "
+HEAD_SEP="$SEP "
+TRAIL_SEP=" $SEP"
+
+echo -n -e "$HEAD_SEP"
 echo -n "States"
 for POPULATION in $POPULATIONS
 do
-	echo -n -e "\t"
+	echo -n -e "$INT_SEP"
 	echo -n $POPULATION
 done
+echo -n -e "$TRAIL_SEP"
 echo
 
 for STATES in $STATES_LIST
 do
+	echo -n -e "$HEAD_SEP"
 	echo -n "$STATES"
 	for POPULATION in $POPULATIONS
 	do
-		echo -n -e "\t"
-		MEAN_MI=$(cat $POPULATION/Chr22Results${STATES}States2/mean-coefficient-of-constraint.txt)
-		echo -n $MEAN_MI
+		echo -n -e "$INT_SEP"
+		MEAN_MI=$(cat ${POPULATION}${SUFFIX}/Chr22Results${STATES}States2/mean-coefficient-of-constraint.txt)
+		echo -n $MEAN_MI | head -c 6
 	done
+	echo -n -e "$TRAIL_SEP"
 	echo
 done
