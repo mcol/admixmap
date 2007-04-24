bfffe574d3f99547851de0ca618d82f65ed6ae0d
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2007-04-24 14:32:17 +0000

    moved admixmap distributed files to dist dir

--- test/admixmap/batchtest.pl
+++ test/admixmap/batchtest.pl
@@ -16,13 +16,13 @@ my $arg_hash = {
     samples                    => 15, 
     burnin                     => 5,
     every                      => 1,
-    locusfile                  => 'data/loci.txt',
-    genotypesfile              => 'data/genotypes.txt',
-    outcomevarfile             => 'data/outcomevars.txt',
-    covariatesfile             => 'data/covariates3std.txt',
+    locusfile                  => '../dist/admixmap/tutorial/data/loci.txt',
+    genotypesfile              => '../dist/admixmap/tutorial/data/genotypes.txt',
+    outcomevarfile             => '../dist/admixmap/tutorial/data//outcomevars.txt',
+    covariatesfile             => '../dist/admixmap/tutorial/data//covariates3std.txt',
     targetindicator            => 0, # diabetes in column 1
     displaylevel             => 2,
-    
+
 # output files
     logfile                    => 'logfile.txt',
     paramfile                  => 'param.txt',
@@ -40,31 +40,31 @@ my $arg_hash = {
 $arg_hash->{thermo} = 1;
 $arg_hash->{numannealedruns} = 4;
 $arg_hash->{populations} = 1;
-#if(doAnalysis($executable,$arg_hash, $resultsdir)){
-#    &CompareThenMove("results", "results0");
-#}
+if(doAnalysis($executable,$arg_hash, $resultsdir)){
+    &CompareThenMove("results", "results0");
+}
 
 # single population, reference prior on allele freqs, annealing  
 $arg_hash->{thermo} = 0;
 $arg_hash->{indadmixhiermodel} = 1;
 $arg_hash->{stratificationtestfile}  = 'strat_test.txt';
-#if(doAnalysis($executable,$arg_hash, $resultsdir)){
-#  &CompareThenMove("results", "results1");
-#}
+if(doAnalysis($executable,$arg_hash, $resultsdir)){
+  &CompareThenMove("results", "results1");
+}
 
 # two populations, reference prior on allele freqs  
 $arg_hash->{numannealedruns} = 0;
 $arg_hash->{populations}     = 2;
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "results2");
-#}
+if(doAnalysis($executable,$arg_hash)){
+    &CompareThenMove("results", "results2");
+}
 
 # fixed allele freqs, individual sumintensities
 # possible problem here - changing numannealedruns should not change output except for annealmon.txt
 delete $arg_hash->{populations};
 delete $arg_hash->{allelefreqoutputfile};
 $arg_hash->{fixedallelefreqs} = 1;
-$arg_hash->{priorallelefreqfile}  = 'data/priorallelefreqs.txt',
+$arg_hash->{priorallelefreqfile}  = '../dist/admixmap/tutorial/data/priorallelefreqs.txt',
 $arg_hash->{allelefreqscorefile}  = 'allelefreqscorefile.txt';
 $arg_hash->{allelefreqscorefile2} = 'allelefreqscorefile2.txt';
 $arg_hash->{ancestryassociationscorefile} = 'ancestryassocscorefile.txt';
@@ -89,22 +89,22 @@ delete $arg_hash->{allelefreqscorefile2};
 delete $arg_hash->{affectedsonlyscorefile};
 $arg_hash->{dispersiontestfile}  = 'dispersiontest.txt';
 $arg_hash->{targetindicator} = 1; # skin reflectance
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "results4");
-#}
+if(doAnalysis($executable,$arg_hash)){
+    &CompareThenMove("results", "results4");
+}
 
 # dispersion model for allele freqs 
 delete $arg_hash->{priorallelefreqfile};
 delete $arg_hash->{dispersiontestfile};
-$arg_hash->{historicallelefreqfile} = 'data/priorallelefreqs.txt';
+$arg_hash->{historicallelefreqfile} = '../dist/admixmap/tutorial/data/priorallelefreqs.txt';
 $arg_hash->{affectedsonlyscorefile}       = 'affectedsonlyscorefile.txt';
 $arg_hash->{fstoutputfile} = 'FSToutputfile.txt';
 $arg_hash->{dispparamfile} = 'disppar.txt';
 $arg_hash->{randommatingmodel} = 0;
 $arg_hash->{targetindicator} = 0; # diabetes
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "results5");
-#}
+if(doAnalysis($executable,$arg_hash)){
+    &CompareThenMove("results", "results5");
+}
 
 # prior on allele freqs, testoneindiv, no regression, thermo
 # possible problem here - changing numannealedruns should not change output except for annealmon.txt
@@ -120,15 +120,15 @@ delete $arg_hash->{outcomes};
 delete $arg_hash->{outcomevarfile};
 delete $arg_hash->{covariatesfile};
 $arg_hash->{testoneindiv} = 1;
-$arg_hash->{priorallelefreqfile}  = 'data/priorallelefreqs.txt',
+$arg_hash->{priorallelefreqfile}  = '../dist/admixmap/tutorial/data/priorallelefreqs.txt',
 $arg_hash->{randommatingmodel} = 1;
 $arg_hash->{globalrho} = 0;
 $arg_hash->{testoneindiv} = 1;
 $arg_hash->{numannealedruns} = 100;
 $arg_hash->{thermo} = 1;
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "results6");
-#}
+if(doAnalysis($executable,$arg_hash)){
+    &CompareThenMove("results", "results6");
+}
 
 # autosomal and X chromosome data
 my $arg_hash = {
@@ -151,9 +151,9 @@ my $arg_hash = {
     #haplotypeassociationscorefile => 'hapassocscore.txt',
     #stratificationtestfile                   => 'strat_test.txt'
 };
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "cattleresults");
-#}
+if(doAnalysis($executable,$arg_hash)){
+    &CompareThenMove("results", "cattleresults");
+}
 
 # Single individual
 my $arg_hash = {
@@ -175,81 +175,10 @@ my $arg_hash = {
     chib                         => 1,
     indadmixturefile             => "indadmixture.txt"
 };
-#if(doAnalysis($executable,$arg_hash)){
-#    &CompareThenMove("results", "Indresults");
-#}
-
-#hapmixmodel
-my $executable = './hapmixmap';
-my $arg_hash = {
-#data files
-
-   genotypesfile                   => 'hapmixdata/genotypes_diploid.txt', #diploid data
-#    genotypesfile                   => 'hapmixdata/genotypes_haploid.txt',#haploid data
-
-    locusfile                       => 'hapmixdata/loci.txt',
-    #priorallelefreqfile             => 'hapmixdata/allelefreqs.txt',
-    #fixedallelefreqs                => 1,
-
-    states=>4,
-#    checkdata=> 0,
-
-#main options
-    resultsdir => 'results',
-    displaylevel   => 3, 
-
-    samples  => 15,
-    burnin   => 5,
-    every    => 1,
-
-numannealedruns => 0,
-thermo => 0,
-hapmixmodel => 1,
-
-hapmixlambdaprior=>"4000, 10, 100, 10",
-#fixedmixtureprops=>1,
-
-allelefreqprior => "1, 1, 1",
-#initiallambdafile => "data/initialambdas.txt",
-#allelefreqfile => "data/initialallelefreqs.txt",
-
-lambdasamplerparams => "0.1, 0.00001, 10, 0.9, 20",
-
-#output files
-    logfile                     => 'logfile.txt',
-    paramfile               => 'paramfile.txt',
-    #regparamfile          => 'regparamfile.txt',
-    #ergodicaveragefile => 'ergodicaverage.txt',
-
-    finalallelefreqfile  => "finalallelefreqs.txt",
-    finallambdafile => "finallambdas.txt",
-    finalfreqpriorfile => "finalfreqpriors.txt",
-
-    hapmixlambdaoutputfile =>"lambdaPosteriorMeans.txt",
-    allelefreqprioroutputfile =>"etaPosteriorMeans.txt",
-
-#optional tests
-#residualallelicassocscorefile => 'residualLDscores.txt',
-mhscoretestfile => 'mhtest.txt',
-    #allelicassociationscorefile       => 'allelicassociationscorefile.txt',
-};
-#diploid data
 if(doAnalysis($executable,$arg_hash)){
-    &CompareThenMove("results", "HapmixDiploidresults");
-}
-#haploid data
-$arg_hash->{genotypesfile} = "hapmixdata/genotypes_haploid.txt";
-if(doAnalysis($executable,$arg_hash)){
-    &CompareThenMove("results", "Hapmixresults");
+    &CompareThenMove("results", "Indresults");
 }
 
-#case-control analysis
-$arg_hash->{ccgenotypesfile} = "hapmixdata/genotypes_casectrl.txt";
-$arg_hash->{outcomevarfile} = "hapmixdata/outcome.txt";
-$arg_hash->{allelicassociationscorefile} = "AllelicAssocScores.txt";
-if(doAnalysis($executable,$arg_hash)){
-    &CompareThenMove("results", "HapmixCCresults");
-}
 
 
 ######################################################################################
