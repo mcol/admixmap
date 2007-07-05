75ea9ffb8094a940140e97ce03c2e1f5586ce403
davidod <davidod@0c12335c-6928-0410-89ea-c6d2b26f428b>
2007-07-05 13:19:23 +0000

    added options to specify location of binary, rscript, function file in hapmixmap tutorial script

--- dist/hapmixmap/tutorial.pl
+++ dist/hapmixmap/tutorial.pl
@@ -5,57 +5,52 @@ use Getopt::Long;
 
 my $function_file = "./doanalysis.pl";
 
-require $function_file or die("cannot find doanalysis.pl");
-
 my $train = '';
 my $test = '';
 my $resume = '';
 my $usage = 0;
+my $executable = "./hapmixmap";
+my $rscript = "./AdmixmapOutput.R";
 
 GetOptions("train" =>\$train,
 	   "test" => \$test,
 	   "resume" => \$resume,
 	   "help!" => \$usage,
+           "function-file=s" => \$function_file,
+           "exec=s"          => \$executable,
+           "rscript=s"       => \$rscript
 	  );
 
-if (not ($train or $resume or $test) or $usage) {
-    print "Usage: $0 [ --train | --resume | --test ]\n";
+if (not ($train or $test) or $usage) {
+    print "Usage: $0 < --train | --test > [--resume] [--exec=...] [--rscript=...] [--function-file=...]\n";
 }
 
-
-my $executable = "./hapmixmap";
-my $rscript = "./AdmixmapOutput.R";
-
+require $function_file or die("cannot find doanalysis.pl");
 
 my $arg_hash = {
-genotypesfile => "data/dn_genotypes.txt",
-locusfile => "data/dn_loci.txt",
-checkdata => 0,
-
-samples => 50,
-burnin => 10,
-every => 1,
-numannealedruns => 0,
-thermo => 0,
-states => 8,
-hapmixmodel =>  1,
-displaylevel => 3,
-
-residualadhiermodel => 0,
-arrivalrateprior => "12, 0.1, 2000,1000",
-residualadprior => "0.25, 1",
-arrivalratesamplerparams => "0.1, 0.00001, 10, 0.9, 20",
-
-resultsdir => "results",
-logfile => "logfile.txt",
-paramfile => "paramfile.txt",
-residualadfile => "allelefreqdispersion.txt",
-
-#allelefreqprioroutputfile => "initialetas.txt",
-#allelefreqoutputfile => "initialallelefreqs.txt",
-#arrivalrateoutputfile => "initiallambdas.txt",
-
-ergodicaveragefile => "ergodicaverage.txt"
+		genotypesfile => "data/dn_genotypes.txt",
+		locusfile => "data/dn_loci.txt",
+		checkdata => 0,
+		
+		samples => 50,
+		burnin => 10,
+		every => 1,
+		numannealedruns => 0,
+		thermo => 0,
+		states => 8,
+		hapmixmodel =>  1,
+		displaylevel => 3,
+		
+		residualadhiermodel => 0,
+		arrivalrateprior => "12, 0.1, 2000,1000",
+		residualadprior => "0.25, 1",
+		arrivalratesamplerparams => "0.1, 0.00001, 10, 0.9, 20",
+		
+		resultsdir => "results",
+		logfile => "logfile.txt",
+		paramfile => "paramfile.txt",
+		residualadfile => "allelefreqdispersion.txt",
+		ergodicaveragefile => "ergodicaverage.txt"
 };
 
 if($train){
