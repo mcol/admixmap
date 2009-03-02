38b11d0e2c15bad4574cdbd0281ebdef2c523c69
dfavro <dfavro@0c12335c-6928-0410-89ea-c6d2b26f428b>
2009-03-02 19:27:09 +0000

    Cosmetics.

--- src/base/Filenames.h
+++ src/base/Filenames.h
@@ -5,7 +5,7 @@
 */
 
 #define PARAMFILE_ROBJECT "paramsAsRobject.txt"
-#define LOCUS_F_VALUES "LocusfValues.txt"
-#define LOCUS_KL_INFO "LocusKLInfo.txt"
+#define LOCUS_F_VALUES	  "LocusfValues.txt"
+#define LOCUS_KL_INFO	  "LocusKLInfo.txt"
 
 #define ALLELE_FREQ_SAMPLER_ACCEPT_RATES  "AlleleFreqSamplerAcceptanceRates.txt"
