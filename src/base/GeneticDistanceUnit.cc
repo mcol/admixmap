6468fdf54e7b5be077d6712afb120c83a9c4ad72
dfavro <dfavro@0c12335c-6928-0410-89ea-c6d2b26f428b>
2009-03-02 19:27:16 +0000

    Moved enum-to-string and string-to-enum from header-file to source-file. Doesn't keep extern-linkage initialized array in header-file. Stores and returns GDU descriptions as "const char *" rather than std::string. Doxygen: added base library classes to group "base"

---
+++ src/base/GeneticDistanceUnit.cc
@@ -0,0 +1,75 @@
+//=============================================================================
+//
+// Copyright (C) 2009  David D. Favro  gpl@meta-dynamic.com
+//
+// This is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License version 3 as published by the Free
+// Software Foundation.
+//
+// This software is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this software; see the file COPYING.  If not, it can be found at
+// http://www.gnu.org/copyleft/gpl.html or by writing to the Free Software
+// Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+//=============================================================================
+
+//=============================================================================
+/// \file GeneticDistanceUnit.cc
+//=============================================================================
+
+#pragma implementation
+#include "GeneticDistanceUnit.h"
+
+#include <stdexcept>
+#include <cstring>  // strcasecmp()
+#include "config.h" // AGGRESSIVE_RANGE_CHECK
+
+
+
+static const char * const descs[] = { "bp", "kb", "Mb", "cM", "M" };
+
+static const int N_DESCS = sizeof(descs) / sizeof(*descs);
+
+
+
+//-----------------------------------------------------------------------------
+// gduAsString()
+//-----------------------------------------------------------------------------
+
+const char * gduAsString( GeneticDistanceUnit u )
+    {
+    #if AGGRESSIVE_RANGE_CHECK
+	if ( N_DESCS != N_GDUS )
+	    throw std::runtime_error( "internal GDU desync" );
+
+	if ( size_t(u) >= N_GDUS )
+	    throw std::runtime_error( "invalid GDU" );
+    #endif
+
+    return descs[ u ];
+    }
+
+
+
+//-----------------------------------------------------------------------------
+// gduFromString()
+//-----------------------------------------------------------------------------
+
+GeneticDistanceUnit gduFromString( const char * desc )
+    {
+    #if AGGRESSIVE_RANGE_CHECK
+	if ( N_DESCS != N_GDUS )
+	    throw std::runtime_error( "internal GDU desync" );
+    #endif
+
+    for ( size_t idx = N_DESCS ; idx-- != 0 ; )
+	if ( strcasecmp( desc, descs[idx] ) == 0 )
+	    return static_cast<GeneticDistanceUnit>( idx );
+
+    throw std::runtime_error( std::string("invalid genetic-distance-unit: \"") + desc + '"' );
+    }
--- src/base/GeneticDistanceUnit.h
+++ src/base/GeneticDistanceUnit.h
@@ -2,9 +2,23 @@
 #ifndef GENETICDISTANCEUNIT_H_
 #define GENETICDISTANCEUNIT_H_
 
+
+/** \addtogroup base
+ * @{ */
+
+
+// Changes to the enum here should be matched in the implementation file
 //!enum for distance units
-enum GeneticDistanceUnit {basepairs, kilobases, megabases, centimorgans, Morgans};
-const std::string GeneticDistanceUnitString[] = {"bp", "kb", "Mb", "cM", "M"};
+enum GeneticDistanceUnit {basepairs, kilobases, megabases, centimorgans, Morgans,
+			  N_GDUS }; ///< This must remain the last tag
+
+const char *	    gduAsString	 ( GeneticDistanceUnit u    );
+GeneticDistanceUnit gduFromString( const char *	       desc );
+
+
+
+/** @} */
+
 
 
 #endif /*GENETICDISTANCEUNIT_H_*/
