788046e426aea2528b6dd99e0ebbb1daca3e1ca2
dfavro <dfavro@0c12335c-6928-0410-89ea-c6d2b26f428b>
2009-03-02 19:27:44 +0000

    Added some documentation and warnings. Cosmetic clean-up of the code. Doxygen: added base library classes to group "base"

--- src/base/common.h
+++ src/base/common.h
@@ -1,6 +1,20 @@
 // *-*-C++-*-*
+
+/// \file common.h
+/// <B>Many</B> standard-library (STL, iostream, etc.) include files are
+/// included from here, as well as some types defined.
+///
+/// @warning
+/// <B>WARNING!</B> this file duplicates, in the global namespace, symbols that
+/// are also defined in @a <bclib/common.h>.  Furthermore, they both use the
+/// same preprocessor symbol (COMMON_H) to protect against multiple includes;
+/// thus changes to either file may not propagate to any particular including
+/// source file.
+
+
 #ifndef COMMON_H
 #define COMMON_H 1
+
 
 #include <vector>
 #include <string>
@@ -10,15 +24,35 @@
 #include <algorithm>
 #include <numeric>
 
-typedef std::vector<std::string> Vector_s; //std vector of strings 
-typedef std::vector<Vector_s>    Matrix_s; // std vector of std vectors
 
-///enum for the various regression types.
+/** \addtogroup base
+ * @{ */
+
+
+typedef std::vector<std::string> Vector_s; ///< std vector of strings
+typedef std::vector<Vector_s>    Matrix_s; ///< std vector of std vectors
+
+/// enum for the various regression types.
 /// Mlinear = Multiple linear, Mlogistic = multiple logistic
-enum RegressionType {None, Linear, Logistic, Cox, Mlinear, Mlogistic, Multiple};
-const std::string RegressionString[] ={"None", "Linear", "Logistic", "Cox", "Mlinear", "Mlogistic", "Multiple"};
+enum RegressionType
+    {
+    None	,
+    Linear	,
+    Logistic	,
+    Cox		,
+    Mlinear	, ///< Multiple linear
+    Mlogistic	, ///< Multiple logistic
+    Multiple	,
+    N_RTYPES	  ///< Keep this as the last symbol, used to count the tags
+    };
+extern const char * getRegressionString( RegressionType ); ///< Gives short readable description
+
 
 ///enum for continuous/binary datatypes
 enum DataType {Continuous, Binary, CoxData};
+
+
+/** @} */
+
 
 #endif /* !defined COMMON_H */
