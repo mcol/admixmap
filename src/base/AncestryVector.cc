7a3cdc36f88aa74e067445a6710fab8e06fed26f
dfavro <dfavro@0c12335c-6928-0410-89ea-c6d2b26f428b>
2009-05-14 16:46:29 +0000

    AncestryVector vastly enhanced, much faster, no dynamic heap memory.     * Internal (data-member) representation rather than deriving from std::vector<>, which dynamically allocates non-shared memory on the heap.  This vastly reduces the time required to make copies, use as return-value, etc.     * Added public methods to support an "index" or numbering of the space, required for probability computations.     * Added optimized "index" cache to lookup the AV corresponding to an index.  Since the cache is static-global, access to it is not thread-safe.  For parallelization via OpenMP, uses thread-private storage.

--- src/base/AncestryVector.cc
+++ src/base/AncestryVector.cc
@@ -31,8 +31,215 @@
 #endif
 
 
+#if PARALLELIZE_TRANS_PROB && defined(_OPENMP)
+    #define THREAD_PRIVATE_CACHE    1
+    #if THREAD_PRIVATE_CACHE
+	#include <omp.h>
+    #endif
+#endif
+
+
 
 namespace genepi { // ----
+
+
+
+//-----------------------------------------------------------------------------
+// Index cache:
+//-----------------------------------------------------------------------------
+
+static unsigned long K_to_the_F( unsigned int K, unsigned int F )
+    {
+    if	    ( K == 2 )
+	return (1U << F);
+    else if ( K == 4 )
+	return (1U << (F << 1));
+    else if ( F == 2 )
+	return (K * K);
+    else
+	{
+	unsigned long rv = 1;
+	while ( F-- != 0 )
+	    rv *= K;
+	return rv;
+	}
+    }
+
+
+namespace
+{  // Begin anonymous namespace to give IdxCache internal linkage
+
+
+// "Static" class used internally to the implementation of AncestryVector:
+class IdxCache
+    {
+    public:
+	typedef AncestryVector::DType DType;
+
+    private:
+
+	PopIdx K; ///< Number of populations
+	struct El
+	    {
+	    size_t  F	  ; ///< Number of founder gametes
+	    size_t  n_els ; ///< K^F, sizeof val vect, cached for range-checking
+	    DType * val   ;
+
+	    El() : val ( 0 ) { }
+	    El( const El & rhs ) : F( rhs.F ), n_els( rhs.n_els ), val( rhs.val ) { const_cast<El&>(rhs).val = 0; }
+	    El& operator=( const El & rhs )
+		{ F = rhs.F; val = rhs.val; const_cast<El&>(rhs).val = 0; return *this; }
+	    ~El() { delete[] val; }
+	    };
+	std::vector<El> els;
+
+    public:
+
+	void reserve( size_t max_F ) { els.reserve( max_F ); }
+
+	IdxCache( PopIdx _K ) : K( _K ) {}
+
+	const DType & lookup( PopIdx _K, size_t F, unsigned long index );
+    };
+
+
+const IdxCache::DType & IdxCache::lookup( PopIdx _K, size_t F, unsigned long index )
+    {
+    gp_assert_eq( _K, K );
+
+    #if 0 // DEBUG, will be checked below for cheaper in one of two places (see *!*!*)
+	gp_assert_lt( index, K_to_the_F(_K,F) );
+    #endif
+
+    for ( std::vector<El>::iterator it = els.begin(); it != els.end(); ++it )
+	if ( F == it->F )
+	    {
+	    gp_assert_lt( index, it->n_els ); // (*!*!*)
+	    return it->val[ index ]; // **** RETURN HERE ****
+	    }
+
+
+
+    // We didn't find an existing entry in the cache with number of
+    // founder-gametes "F", so we'll create one and populate it.  Since this
+    // involves modifying the (shared) cache, we mark this section as critical.
+
+    const DType * rv;
+
+    #if PARALLELIZE_TRANS_PROB && defined(_OPENMP) && (! THREAD_PRIVATE_CACHE)
+      #pragma omp critical
+	{
+    #endif
+
+    #define MOD_IN_PLACE 0
+    #if MOD_IN_PLACE
+	els.resize( els.size() + 1 );
+	El & newEl = els.back();
+    #else
+	El newEl;
+    #endif
+
+    newEl.F = F;
+    const size_t size = K_to_the_F( K, F );
+    newEl.n_els = size;
+
+    newEl.val = new DType[ size ];
+    memset( newEl.val, '\0', size );
+
+    #if 0
+	DType * last = newEl.val + (size-1); // **** DEBUG ****
+    #endif
+    DType * ptr = newEl.val;
+    for ( size_t x = size ; --x != 0 ; )
+	{
+	DType & prev = *ptr;
+	DType & next = *++ptr;
+	next = prev;
+	#if 0
+	    gp_assert_eq( prev.size(), F ); // **** DEBUG ****
+	    gp_assert_eq( next.size(), F ); // **** DEBUG ****
+	    fprintf( stderr, "-> %lu %lu %p %p %p\n", size, x, &prev, &next, last ); // **** DEBUG ****
+	#endif
+
+	// ---- begin "increment" operation ----
+	size_t curEl = 0;
+	PopIdx val = next.at_unsafe( curEl );
+
+	while ( ++val == _K )
+	    {
+	    next.setAt_unsafe( curEl++, 0 );
+	    gp_assert_lt( curEl, F );
+	    val = next.at_unsafe( curEl );
+	    }
+	next.setAt_unsafe( curEl, val );
+	// ---- end "increment" operation ----
+	}
+
+    gp_assert_lt( index, size ); // (*!*!*)
+    #if 0
+	gp_assert_eq( newEl.val[index].size(), F ); // **** DEBUG ****
+    #endif
+    rv = &( newEl.val[ index ] );
+
+    #if ! MOD_IN_PLACE
+	els.push_back( newEl );
+    #endif
+
+    #if PARALLELIZE_TRANS_PROB && defined(_OPENMP) && (! THREAD_PRIVATE_CACHE)
+      } // End critical section
+    #endif
+
+    return *rv;
+    }
+
+
+} // End anonymous namespace to give IdxCache internal linkage
+
+
+
+//-----------------------------------------------------------------------------
+// set_ulong()
+// WARNING:  NOT THREAD-SAFE!!!  USES UNPROTECTED GLOBAL CACHE!!!
+// We protected for OpenMP, when PARALLELIZE_TRANS_PROB is turned on.
+//-----------------------------------------------------------------------------
+
+#if PARALLELIZE_TRANS_PROB && defined(_OPENMP)
+
+    #if THREAD_PRIVATE_CACHE
+	static IdxCache * cache = 0;
+	void AncestryVector::set_K( PopIdx /*K*/ )
+	    {
+	    omp_set_dynamic( false ); // Explicitly turn off dynamic threads
+	    }
+    #else
+	static IdxCache * cache = 0;
+	void AncestryVector::set_K( PopIdx K )
+	    {
+	    gp_assert( cache == 0 );
+	    cache = new IdxCache( K );
+	    #if ! THREAD_PRIVATE_CACHE
+		cache->reserve( AV_MAX_FOUNDER_GAMETES );
+		for ( size_t F = 1 ; F < 6 ; ++F )
+		    cache->lookup( K, F, 0 );
+	    #endif
+	    }
+    #endif
+
+#endif
+
+void AncestryVector::set_ulong( unsigned long nv )
+    {
+    #if ! (defined(_OPENMP) && PARALLELIZE_TRANS_PROB)
+	static IdxCache _cache( K );
+	#define cache (&_cache)
+    #elif THREAD_PRIVATE_CACHE
+	#pragma omp threadprivate( cache )
+	if ( cache == 0 )
+	    cache = new IdxCache( K );
+    #endif
+
+    memcpy( data.bytes, cache->lookup(K, ped.getNFounderGametes(), nv).bytes, AV_MC_BYTES() );
+    }
 
 
 
@@ -45,15 +252,15 @@ unsigned long AncestryVector::to_ulong() const
     unsigned long rv = 0;
 
     if ( K == 2 )	// Optimized version for K==2:
-	for ( size_t idx = size() ; idx-- != 0 ; )
+	for ( IdxType idx = size() ; idx-- != 0 ; )
 	    rv = (rv << 1) + at(idx);
 
     else if ( K == 3 )	// Optimized version for K==3:
-	for ( size_t idx = size() ; idx-- != 0 ; )
+	for ( IdxType idx = size() ; idx-- != 0 ; )
 	    rv = (rv << 1) + rv + at(idx);
 
     else
-	for ( size_t idx = size() ; idx-- != 0 ; )
+	for ( IdxType idx = size() ; idx-- != 0 ; )
 	    rv = (rv * K) + at(idx);
 
 
@@ -94,8 +301,8 @@ unsigned long AncestryVector::to_ulong() const
 	gp_assert( av.size() != 0 );
 
 	os << "AV(";
-	const size_t limit = av.size() - 1;
-	for ( size_t idx = 0 ; idx < limit ; ++idx )
+	const AncestryVector::IdxType limit = av.size() - 1;
+	for ( AncestryVector::IdxType idx = 0 ; idx < limit ; ++idx )
 	    output( os, av.at(idx) ) << ',';
 	os << av.at(limit) << ')';
 
--- src/base/AncestryVector.h
+++ src/base/AncestryVector.h
@@ -31,11 +31,16 @@
 #include "Pedigree.h"
 #include "Organism.h"	// PopIdx
 #include "exceptions.h"
-#include <vector>
+
+#include <cstring> // memcpy()
 
 
+/// Should computation of transition probabilities be parallelized (requires
+/// thread-safety for AncestryVector::set_ulong()).  This only has any effect if
+/// TPC_CACHE_MODEL==TPC_BIG_CACHE
+#define PARALLELIZE_TRANS_PROB	1
 
-#define AV_OSTREAM		1 /// Should we compile ostream support for AVs?
+#define AV_OSTREAM		1 ///< Should we compile ostream support for AVs?
 
 #if AV_OSTREAM
     #include <iosfwd>
@@ -43,7 +48,8 @@
 
 
 
-#define AV_STORAGE unsigned char // PopIdx
+#define AV_MAX_FOUNDER_GAMETES	16 ///< Should be even
+#define AV_MAX_K		16
 
 
 
@@ -53,81 +59,179 @@ namespace genepi { // ----
 
 //-----------------------------------------------------------------------------
 //
+/// Stores an ancestry vector for a pedigree: one element for each founder
+/// gamete; each element is a population (PopIdx), a number from 0 to K-1.
+///
 /// Since we may be storing many of these vectors in memory, we'll optimize the
 /// internal storage of the elements as "unsigned char", leading to a maximum of
 /// 256 populations; but externally we present them as PopIdx (typically
 /// size_t).  This leads to private inheritance from std::vector.
 ///
-/// NOTE *2*: we could consider exclusively indexing based on the founder-index
-///	and which gamete; i.e. using (Pedigree::FounderIdx,MaternalPaternalType)
-///	to reference the elements.
+/// <A name="note-1"></A>
+/// NOTE *1*: We need to index both based on the founder-index and which gamete
+/// (i.e. using {Pedigree::FounderIdx,MaternalPaternalType}) for inheritance,
+/// and by a simple integral index (IdxType) for comparison and probability
+/// computation.  We might consider an iterator rather than the index externally.
 //
 //-----------------------------------------------------------------------------
 
-class AncestryVector : private std::vector<AV_STORAGE>
+//template < size_t MAX_FNDR_GAMETES > class AncestryVectorT
+class AncestryVector
     {
+    public:
+	/// Integer index into the elements, i.e. founder-gamete-index:
+	typedef size_t IdxType;
+
+    protected:
+
+	typedef unsigned char WordType;
+	static const size_t MAX_BYTES = (AV_MAX_FOUNDER_GAMETES + 1) >> 1;
+
+    public:
+	/// We publicize this internal component only so that we can use it from
+	/// the IdxCache class that is internal to the implementation, without
+	/// making IdxCache a friend/member of AncestryVector.  DType is not
+	/// usable from outside AncestryVector.
+	struct DType
+	    {
+	    WordType bytes[ MAX_BYTES ];
+
+	    PopIdx at_unsafe( IdxType el ) const
+		{
+		const WordType byte = bytes[ el >> 1 ];
+		return ((el&1) != 0) ? (byte >> 4) : (byte & 0x0F);
+		}
+
+	    void setAt_unsafe( IdxType el, PopIdx val )
+		{
+		WordType & byte = bytes[ el >> 1 ];
+
+		if ( (el & 1) == 0 )
+		    { // Lower-order nibble
+		    byte &= 0xF0;
+		    byte |= val;
+		    }
+		else
+		    { // Higher-order nibble
+		    byte &= 0x0F;
+		    byte |= (val << 4);
+		    }
+		}
+
+	    };
+
+
     private:
 	const Pedigree & ped;
 	const PopIdx	 K;
+	DType		 data;
 
-	typedef std::vector<AV_STORAGE> SUPER;
-
+	/// Maybe we should cache this somewhere.
+	size_t nBytes() const { return ((size()+1) >> 1); }
+	#if 1
+	    #define AV_MC_BYTES() MAX_BYTES
+	#else
+	    #define AV_MC_BYTES() nBytes()
+	#endif
 
     protected:
-	/// See NOTE *2*:
-	static size_t idxOf( Pedigree::FounderIdx f, bool isPaternalGamete )
+	/// Converts between (founder-index,which-gamete) and
+	/// founder-gamete-index (see <A HREF="#note-1">NOTE *1*</A>).
+	static IdxType idxOf( Pedigree::FounderIdx f, bool isPaternalGamete )
 	    {
-	    size_t idx = f << 1;
+	    IdxType idx = f << 1;
 	    if ( isPaternalGamete )
 		++idx;
 	    return idx;
 	    }
 
+	AncestryVector( const Pedigree & _ped, PopIdx _K, const DType & val ) :
+		ped   ( _ped ) ,
+		K     ( _K   )
+	    {
+	    memcpy( data.bytes, val.bytes, AV_MC_BYTES() );
+	    }
+
     public:
 	AncestryVector( const Pedigree & _ped, PopIdx _K ) :
-	    SUPER ( (_ped.getNFounders() << 1)  ) ,
-	    ped	  ( _ped			) ,
-	    K	  ( _K				) {}
+		ped ( _ped ) ,
+		K   ( _K   ) {}
 
 	AncestryVector( const AncestryVector & rhs ) :
-	    SUPER ( rhs		) ,
-	    ped	  ( rhs.ped	) ,
-	    K	  ( rhs.K	) {}
+		ped ( rhs.ped ) ,
+		K   ( rhs.K   )
+	    {
+	    memcpy( data.bytes, rhs.data.bytes, AV_MC_BYTES() );
+	    }
 
 
 	/// For use as an index into arrays; in the range of (0,K^F)
 	unsigned long to_ulong() const;
 
+	/// @warning Not thread-safe!!!  This method uses an unprotected global
+	/// cache.  We take explicit precautions for the OpenMP facility if
+	/// PARALLELIZE_TRANS_PROB is turned on, but this method will not be
+	/// compatible with other multi-threaded environments.
+	void set_ulong( unsigned long nv );
 
-	size_t size() const { return SUPER::size(); } ///< Publicized vector method
-	PopIdx at( size_t el ) const		      ///< Element access (publicized vector method)
+
+
+	//---------------------------------------------------------------------
+	// Index (founder-gamete-index) based read-access to the elements:
+	//---------------------------------------------------------------------
+
+	/// Number of elements (founder-gamete-indexed).
+	/// Maybe we should cache this somewhere.
+	IdxType size() const { return ped.getNFounderGametes(); }
+	PopIdx at_unsafe( IdxType el ) const		///< at(IdxType), but not range-checked
+	    { return data.at_unsafe(el); }
+	PopIdx at( IdxType el ) const			///< Element access via founder-gamete-index
 	    {
 	    gp_assert_lt( el, size() );
-	    return SUPER::operator[](el);
+	    return at_unsafe( el );
 	    }
-	PopIdx operator[]( size_t el ) const { return at(el); } ///< equivalent to at(size_t)
+	PopIdx operator[]( IdxType el ) const { return at(el); } ///< equivalent to at(IdxType)
 
-	/// See NOTE *2*.  @see { at(size_t) }
+	/// See (see <A HREF="#note-1">NOTE *1*</A>) and at(IdxType)
 	PopIdx at( Pedigree::FounderIdx f, bool isPaternalGamete ) const
 	    {
 	    return at( idxOf(f,isPaternalGamete) );
 	    }
 
 
-	/// Set the ancestry at position @a el to @a val
-	void setAt( size_t el, PopIdx val )
+	//---------------------------------------------------------------------
+	// Index (founder-gamete-index) based write-access to the elements:
+	//---------------------------------------------------------------------
+
+	/// Like @link setAt(IdxType,PopIdx) setAt() @endlink, but not range-checked.
+	void setAt_unsafe( IdxType el, PopIdx val )
+	    { return data.setAt_unsafe( el, val ); }
+
+	/// Set the ancestry at position @a el to @a val.
+	void setAt( IdxType el, PopIdx val )
 	    {
 	    gp_assert_lt( el , size() );
-	    gp_assert_lt( val, K      );
-	    SUPER::operator[](el) = val;
+	    gp_assert_lt( val, K );
+	    setAt_unsafe( el, val );
 	    }
 
-	/// See NOTE *2*
+	/// See (see <A HREF="#note-1">NOTE *1*</A>) and setAt(IdxType,PopIdx).
 	void setAt( Pedigree::FounderIdx f, bool isPaternalGamete, PopIdx val )
 	    {
 	    setAt( idxOf(f,isPaternalGamete), val );
 	    }
+
+
+	#if PARALLELIZE_TRANS_PROB && defined(_OPENMP)
+	    /// Hack!  Must be called prior to first call to set_ulong().
+	    static void set_K( PopIdx K );
+	#endif
+
     };
+
+
+
+//typedef AncestryVectorT<AV_MAX_FOUNDER_GAMETES> AncestryVector;
 
 
 
