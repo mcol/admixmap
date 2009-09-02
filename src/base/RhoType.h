2d285d960e276574900e417723b1947b9a728a23
dfavro <dfavro@0c12335c-6928-0410-89ea-c6d2b26f428b>
2009-09-02 12:27:42 +0000

    Created RhoType to name the datatype of various rho's. Reparented to bclib::cvector from std::vector.

---
+++ src/base/RhoType.h
@@ -0,0 +1,57 @@
+//=============================================================================
+//
+// Copyright (C) 2009  David D. Favro  gpl-copyright@meta-dynamic.com
+//
+// This is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License version 3 as published by the Free
+// Software Foundation.
+//
+// This software is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this software; see the file COPYING.  If not, it can be found at
+// http://www.gnu.org/copyleft/gpl.html or by writing to the Free Software
+// Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+//=============================================================================
+
+//=============================================================================
+/// \file RhoType.h
+/// Definition of RhoType.
+//=============================================================================
+
+
+#ifndef __base_RhoType_h
+#define __base_RhoType_h
+
+
+
+#include <bclib/cvector.h>
+
+
+
+namespace genepi { // ----
+
+
+/** \addtogroup base
+ * @{ */
+
+
+
+/// Type definition for "rho".  Vector of doubles, indexed on ???
+typedef cvector< double > RhoType;
+
+
+
+
+/** @} */
+
+
+} // ---- end namespace genepi
+
+
+
+#endif // ! __base_RhoType_h
